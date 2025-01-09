c29e85a96016184f9a0fac2b501643fbe6f887d2
josh-reynolds <josh.reynolds@frontier.com>
2025-01-08 16:40:10 -0800

    Adding arrow key control to last tutorial

--- pygame/tutorials/rect_2.py
+++ pygame/tutorials/rect_2.py
@@ -11,11 +11,13 @@ BLACK  = (  0,   0,   0)
 SIZE   = (500, 200)
 
 width, height = SIZE
+dirs = {K_LEFT: (-5, 0), K_RIGHT: (5, 0), K_UP: (0, -5), K_DOWN: (0, 5)}
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE)
 
-rect = Rect(50, 60, 200, 80)
+rect0 = Rect(50, 60, 200, 80)
+rect = rect0.copy()
 
 running = True
 while running:
@@ -36,9 +38,13 @@ while running:
                 rect.centery = height//2
             if event.key == K_b:
                 rect.bottom = height
+            if event.key in dirs:
+                v = dirs[event.key]
+                rect.move_ip(v)
 
     screen.fill(GREY)
-    pygame.draw.rect(screen, BLUE, rect)
+    pygame.draw.rect(screen, BLUE, rect0, 1)
+    pygame.draw.rect(screen, RED, rect, 4)
     pygame.display.flip()
 
 pygame.quit()
