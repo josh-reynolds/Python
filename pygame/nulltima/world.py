6da8d1798388fe67d471de5a7dae6ff4699291c3
josh-reynolds <josh.reynolds@frontier.com>
2025-01-10 12:57:25 -0800

    Adding contents field to World

--- pygame/nulltima/world.py
+++ pygame/nulltima/world.py
@@ -2,18 +2,20 @@ import unittest
 from common import *
 
 class World:
-    def __init__(self):
-        pass
+    def __init__(self, contents):
+        self.contents = contents
 
     def get_cell(self, x, y):
         return BLUE
 
-def world():
-    return World()
+def world(contents=[]):
+    return World(contents)
 
 class WorldTestCase(unittest.TestCase):
     def test_constructing_a_world(self):
-        w = world()
+        contents = [1]
+        w = world(contents)
+        self.assertEqual(w.contents, [1])
 
 # ---------------------------------------------------------------------------
 if __name__ == '__main__':
