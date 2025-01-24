df0ecdba719e8c25220f9fe99d375f982fe8ba9f
josh-reynolds <josh.reynolds@frontier.com>
2025-01-23 22:00:13 -0800

    Adding blinking cursor to Console class

--- pygame/nulltima/game.py
+++ pygame/nulltima/game.py
@@ -81,10 +81,14 @@ class Console:
         self.fontname = None
         self.fontsize = 24
         self.fontcolor = Color('black')
+        self.bg = Color('cadetblue3')
         self.set_font()
         self.lines = []
         self.maxlines = 5
         self.prompt = '|>  '
+        self.prompt_pos = 0
+        self.prompt_color = self.bg
+        self.time = 0
         self.add()
         self.render()
 
@@ -94,9 +98,10 @@ class Console:
     def render(self):
         self.width = 200
         self.img = pygame.Surface((self.width, 20 + self.maxlines * self.fontsize))
-        self.img.fill(Color('cadetblue3'))
+        self.img.fill(self.bg)
         self.rect = self.img.get_rect()
         self.rect.topleft = self.pos
+        self.prompt_pos = (len(self.lines) - 1) * self.fontsize + 5
 
         for i,line in enumerate(self.lines):
             text = self.font.render(line, True, self.fontcolor)
@@ -111,11 +116,20 @@ class Console:
 
     def draw(self):
         self.render()
+        cursor_rect = (30, self.prompt_pos, self.fontsize/6, self.fontsize)
+        pygame.draw.rect(self.img, self.prompt_color, cursor_rect)
         Game.screen.blit(self.img, self.rect)
 
     def update(self):
         if len(self.lines) > self.maxlines:
             self.lines.pop(0)
+        self.time += 1
+        if self.time > 10:
+            self.time = 0
+            if self.prompt_color == self.bg:
+                self.prompt_color = self.fontcolor
+            else:
+                self.prompt_color = self.bg
 
 class Level:
     options = {
