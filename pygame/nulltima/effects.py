79fda71c1dcaa4d40d5520c584eda08fa0d4b72f
josh-reynolds <josh.reynolds@frontier.com>
2025-02-03 16:52:03 -0800

    Adding a sound effect to attack animation

--- pygame/nulltima/effects.py
+++ pygame/nulltima/effects.py
@@ -17,6 +17,8 @@ class MeleeAttack(Effect):
         self.image = MeleeAttack.image
         self.time = 0
         self.lifespan = 20
+        self.sound = pygame.mixer.Sound('./audio/slap.wav')
+        self.played = False
     
     def update(self):
         self.time += 1
@@ -27,8 +29,10 @@ class MeleeAttack(Effect):
         if self.level.grid.can_view(self.pos):
             grid_coord = self.level.grid.from_world(self.pos)
             screen_coord = self.level.grid.to_screen(grid_coord)
-            #game.Game.screen.blit(self.images[self.current_image], screen_coord)
             game.Game.screen.blit(self.image, screen_coord)
+            if not self.played:           # we only want the sound effect
+                self.sound.play()         # on the first animation frame
+                self.played = True
 
 
 
