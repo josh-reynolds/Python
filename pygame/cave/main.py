237df895d388ca356cd58591f46b96c90548c6c7
josh-reynolds <josh.reynolds@frontier.com>
2025-02-11 18:07:13 -0800

    Adding update() function and necessary stubs

--- pygame/cave/main.py
+++ pygame/cave/main.py
@@ -14,17 +14,48 @@ HEIGHT = 480
 TITLE = "Cave"
 
 
+class Game():
+    def __init__(self):
+        pass
 
+    def update(self):
+        pass
 
+def space_pressed():
+    pass
 
-
-
+class State(Enum):
+    MENU = 1
+    PLAY = 2
+    GAME_OVER = 3
 
 def update():
-    pass
+    global state, game
+
+    if state == State.MENU:
+        if space_pressed():
+            state = State.PLAY
+            game = Game(Player())
+        else:
+            game.update()
+        
+    elif state == State.PLAY:
+        if game.player.lives < 0:
+            game.play_sound("over")
+            state = State.GAME_OVER
+        else:
+            game.update()
+
+    elif state == State.GAME_OVER:
+        if space_pressed():
+            state = State.MENU
+            game = Game()
 
 def draw():
     pass
+
+state = State.MENU
+game = Game()
 
 #---------------------------------
 from engine import run
