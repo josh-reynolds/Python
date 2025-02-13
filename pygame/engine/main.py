2da5aeadce60dc546591762d70a274664417621d
josh-reynolds <josh.reynolds@frontier.com>
2025-02-13 12:16:17 -0800

    Minimal starting script for the engine

---
+++ pygame/engine/main.py
@@ -0,0 +1,20 @@
+import pygame
+from engine import Actor, music, keyboard, sounds
+
+WIDTH = 800
+HEIGHT = 480
+TITLE = "Test Bed"
+
+
+def update():
+    pass
+
+def draw():
+    pass
+
+
+
+
+
+from engine import run
+run()
