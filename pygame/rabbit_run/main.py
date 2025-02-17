0ca2fa5e32f67a459710908c687a8d29d455b0a4
josh-reynolds <josh.reynolds@frontier.com>
2025-02-16 20:10:29 -0800

    Filling in stub for update()

--- pygame/rabbit_run/main.py
+++ pygame/rabbit_run/main.py
@@ -5,9 +5,18 @@ WIDTH = 800
 HEIGHT = 480
 TITLE = "Run Rabbit Run"
 
+class keys:
+    SPACE = None
+
+def key_just_pressed(x):
+    pass
+
 class Game():
     def __init__(self):
         self.scroll_pos = 1
+
+    def update(self):
+        pass
 
     def draw(self):
         pass
@@ -18,7 +27,34 @@ class State(Enum):
     GAME_OVER = 3
 
 def update():
-    pass
+    global state, game, high_score
+
+    if state == State.MENU:
+        if key_just_pressed(keys.SPACE):
+            state = State.PLAY
+            game = Game(Rabbit((240, -320)))
+        else:
+            game.update()
+
+    elif state == State.PLAY:
+        if game.rabbit.state != PlayerState.ALIVE and game.rabbit.timer < 0:
+            high_score = max(high_score, game.score())
+
+            try:
+                with open("high.txt", "w") as file:
+                    file.write(str(high_score))
+            except:
+                pass
+
+            state = State.GAME_OVER
+        else:
+            game.update()
+
+    elif state == State.GAME_OVER:
+        if key_just_pressed(keys.SPACE):
+            game.stop_looped_sounds()
+            state = State.MENU
+            game = Game()
 
 def draw():
     game.draw()
