dbc94dbde1ebb9c9f3e3dc7a9b1815ad9758c46a
josh-reynolds <josh.reynolds@frontier.com>
2025-02-16 19:43:50 -0800

    Filling in stub for draw()

--- pygame/rabbit_run/main.py
+++ pygame/rabbit_run/main.py
@@ -1,16 +1,42 @@
-
+from enum import Enum
 
 
 WIDTH = 800
 HEIGHT = 480
 TITLE = "Run Rabbit Run"
 
+class Game():
+    def __init__(self):
+        self.scroll_pos = 1
+
+    def draw(self):
+        pass
+
+class State(Enum):
+    MENU = 1,
+    PLAY = 2,
+    GAME_OVER = 3
 
 def update():
     pass
 
 def draw():
-    pass
+    game.draw()
+
+    if state == State.MENU:
+        screen.blit('title', (0,0))
+        screen.blit('start' + str([0,1,2,1][game.scroll_pos // 6 % 4]),
+                    ((WIDTH - 270) // 2, HEIGHT - 240))
+
+    elif state == State.PLAY:
+        display_number(game.score(), 0, 0, 0)
+        display_number(high_score, 1, WIDTH - 10, 1)
+
+    elif state == State.GAME_OVER:
+        screen.blit("gameover", (0,0))
+
+state = State.MENU
+game = Game()
 
 #-----------------------
 from engine import run
