170601608c42b1f111400b0748d604dd1d4a73c2
josh-reynolds <josh.reynolds@frontier.com>
2025-02-23 21:45:31 -0800

    Filling in stub for Game.update()

--- pygame/bugz/main.py
+++ pygame/bugz/main.py
@@ -1,14 +1,39 @@
 from enum import Enum
+from random import random, randint
 from engine import keyboard
 
-WIDTH = 480
-HEIGHT = 800
+WIDTH = 480    ###
+HEIGHT = 800    ###
 TITLE = "Bugz!"
 
-num_grid_rows = 10
-num_grid_cols = 10
+DEBUG_TEST_RANDOM_POSITIONS = True    ###
 
-class Game():
+num_grid_rows = 10    ###
+num_grid_cols = 10    ###
+
+class FlyingEnemy:
+    def __init__(self, a):
+        self.health = 1     ###
+        self.x = 1          ###
+
+    def update(self):
+        pass
+
+class Segment:
+    def __init__(self, a, b, c, d, e):
+        self.health = 1     ###
+
+    def update(self):
+        pass
+
+class Rock:
+    def __init__(self, a, b):
+        pass
+
+    def update(self):
+        pass
+
+class Game:
     def __init__(self, player=None):
         self.wave = -1
         self.time = 0
@@ -21,9 +46,61 @@ class Game():
         self.score = 0
 
     def update(self):
-        pass
+        self.time += (2 if self.wave % 4 == 3 else 1)
+        self.occupied = set()
+        all_objects = sum(self.grid, self.bullets + self.segments +
+                          self.explosions + [self.player] + [self.flying_enemy])
+
+        for obj in all_objects:
+            if obj:
+                obj.update()
+
+        self.bullets = [b for b in self.bullets if b.y > 0 and not b.done]
+        self.explosions = [e for e in self.explosions if not e.timer == 31]
+        self.segments = [s for s in self.segments if s.health > 0]
+
+        if self.flying_enemy:
+            if self.flying_enemy.health <= 0 or self.flying_enemy.x < -35 \
+                    or self.flying_enemy.x > 515:
+                        self.flying_enemy = None
+        elif random() < 0.01:
+            self.flying_enemy = FlyingEnemy(self.player.x if self.player else 240)
+
+        if self.segments == []:
+            num_rocks = 0
+            for row in self.grid:
+                for element in row:
+                    if element != None:
+                        num_rocks += 1
+            if num_rocks < 31 + self.wave:
+                while True:
+                    x, y = randint(0, num_grid_cols-1), randint(1, num_grid_rows-1)
+                    if self.grid[y][x] == None:
+                        self.grid[y][x] = Rock(x, y)
+                        break
+            else:
+                game.play_sound("wave")
+                self.wave += 1
+                self.time = 0
+                self.segments = []
+                num_segments = 8 + self.wave // 4 * 2
+                for i in range(num_segments):
+                    if DEBUG_TEST_RANDOM_POSITIONS:
+                        cell_x, cell_y = randint(1, 7), randint(1, 7)
+                    else:
+                        cell_x, cell_y = -1-i, 0
+
+                    health = [[1,1],[1,2],[2,2],[1.1]][self.wave % 4][i % 2]
+                    fast = self.wave % 4 == 3
+                    head = i == 0
+                    self.segments.append(Segment(cell_x, cell_y, health, fast, head))
+
+        return self
 
     def draw(self):
+        pass
+
+    def play_sound(self, a):
         pass
 
 def space_pressed():
