dc92e4fe6fbc0fc575b4708550c8177afb5ea81a
josh-reynolds <josh.reynolds@frontier.com>
2025-02-28 11:40:30 -0800

    Filling in Controls ctor stub

--- pygame/futbol/main.py
+++ pygame/futbol/main.py
@@ -35,8 +35,20 @@ def key_just_pressed(key):
     return result
 
 class Controls:
-    def __init__(self, a, b=None):
-        pass
+    def __init__(self, player_num):
+        if player_num == 0:
+            self.key_up = keys.UP
+            self.key_down = keys.DOWN
+            self.key_left = keys.LEFT
+            self.key_right = keys.RIGHT
+            self.key_shoot = keys.SPACE
+        else:
+            self.key_up = keys.W
+            self.key_down = keys.S
+            self.key_left = keys.A
+            self.key_right = keys.D
+            self.key_shoot = keys.LSHIFT
+
 
 class State(Enum):
     MENU = 0,
