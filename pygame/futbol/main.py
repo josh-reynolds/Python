9e775f37c1d06bac3edbbc39a264a405440cf23b
josh-reynolds <josh.reynolds@frontier.com>
2025-03-02 10:43:04 -0800

    Filling in Controls.move() stub

--- pygame/futbol/main.py
+++ pygame/futbol/main.py
@@ -564,8 +564,17 @@ class Controls:
             self.key_right = keys.D
             self.key_shoot = keys.LSHIFT
 
-    def move(self, a):
-        return Vector2(0,0)                         ####
+    def move(self, speed):
+        dx, dy = 0,0
+        if keyboard[self.key_left]:
+            dx = -1
+        elif keyboard[self.key_right]:
+            dx = 1
+        if keyboard[self.key_up]:
+            dy = -1
+        elif keyboard[self.key_down]:
+            dy = 1
+        return Vector2(dx,dy) * speed
 
     def shoot(self):
         return key_just_pressed(self.key_shoot)
