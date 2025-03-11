f171ec91c71faab733deafeb24ac6f479f56d940
josh-reynolds <josh.reynolds@frontier.com>
2025-03-10 19:15:14 -0700

    Filling in draw() stub

--- pygame/brikz/main.py
+++ pygame/brikz/main.py
@@ -4,10 +4,27 @@ WIDTH = 800
 HEIGHT = 480
 TITLE = 'Brikz'
 
+class Game:
+    def draw(self):
+        pass
+
+class State:
+    TITLE = None           ###
+
 def update():
     pass
 
 def draw():
-    pass
+    game.draw()
+    if state == State.TITLE:
+        screen.blit("title", (0,0))
+        screen.blit("startgame", (20,80))
+        screen.blit(f"start{(total_frames // 4) % 13}", (WIDTH//2 - 250//2, 530))
+    elif state == State.GAME_OVER:
+        screen.blit(f"gameover{(total_frames // 4) % 15}", (WIDTH//2 - 450//2, 450))
+
+state = State.TITLE
+game = Game()         ###
+total_frames = 0
 
 run()
