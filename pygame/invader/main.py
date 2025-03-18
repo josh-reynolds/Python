e8451168b1debb6a049b47ea2d1735096eb4f8f8
josh-reynolds <josh.reynolds@frontier.com>
2025-03-18 12:41:49 -0700

    Filling in stub for draw()

--- pygame/invader/main.py
+++ pygame/invader/main.py
@@ -1,15 +1,31 @@
+from enum import Enum
 from engine import *
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 960
+HEIGHT = 540
 TITLE = "Invader"
 
+
+class State(Enum):
+    TITLE = 1,
+    ####
 
 def update():
     pass
 
 def draw():
-    pass
+    if state == State.TITLE:
+        screen.blit("title", (0,0))
+        screen.blit(f"start{(state_timer // 4) % 14}", (WIDTH // 2 - 350 // 2, 450))
 
+    elif state == State.PLAY:
+        game.draw()
+
+    elif state == State.GAME_OVER:
+        game.dxraw()
+        draw_text("GAME OVER", WIDTH // 2, (HEIGHT // 2) - 100, True)
+
+state = State.TITLE
+state_timer = 0
 
 run()
