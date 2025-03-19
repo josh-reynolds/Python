88ae2d98c69778695215d74c8cb8b99eab43c483
josh-reynolds <josh.reynolds@frontier.com>
2025-03-19 12:06:21 -0700

    Filling in update() stub

--- pygame/invader/main.py
+++ pygame/invader/main.py
@@ -5,13 +5,47 @@ WIDTH = 960
 HEIGHT = 540
 TITLE = "Invader"
 
+class KeyboardControls:         ###
+    def update(self):
+        pass                        ###
+    def button_pressed(self, a):    ###
+        pass                         ###
 
 class State(Enum):
     TITLE = 1,
     ####
 
 def update():
-    pass
+    global state, game, state_timer
+
+    keyboard_controls.update()
+
+    state_timer += 1
+
+    if state == State.TITLE:
+        for controls in (keyboard_controls,):     # comments indicate GitHub has controller support too
+            if controls is not None and controls.button_pressed(0):
+                state = State.PLAY
+                state_timer = 0
+                game = Game(Player(controls))
+                break
+
+    elif state == State.PLAY:
+        if game.player.lives <= 0:
+            state = State.GAME_OVER
+            state_timer = 0
+        else:
+            game.update()
+
+    elif state == State.GAME_OVER:
+        game.update()
+        if state_timer > 60:
+            for controls in (keyboard_controls,):     # comments indicate GitHub has controller support too
+                if controls is not None and controls.button_pressed(0):
+                    state = State.TITLE
+                    state_timer = 0
+                    game = None
+                    play_music("menu_theme")
 
 def draw():
     if state == State.TITLE:
@@ -24,6 +58,8 @@ def draw():
     elif state == State.GAME_OVER:
         game.dxraw()
         draw_text("GAME OVER", WIDTH // 2, (HEIGHT // 2) - 100, True)
+
+keyboard_controls = KeyboardControls()
 
 state = State.TITLE
 state_timer = 0
