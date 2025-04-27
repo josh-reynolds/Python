2849baed3fc315a2d9f78390ebe5ca00775a8c80
josh-reynolds <josh.reynolds@frontier.com>
2025-04-26 17:01:18 -0700

    Filling in update() stub

--- pygame/racer/main.py
+++ pygame/racer/main.py
@@ -7,9 +7,15 @@ WIDTH = 400
 HEIGHT = 400
 TITLE = "Racer"
 
+FIXED_TIMESTEP = 1 ###
+
 SPECIAL_FONT_SYMBOLS = {'xb_a':'%'}
 
 fade_to_black_image = pygame.Surface((WIDTH,HEIGHT))
+
+class KeyboardControls:
+    def button_pressed(self, a):  ###
+        pass ###
 
 class Game:
     def draw(self):
@@ -17,12 +23,53 @@ class Game:
 
 def draw_text(a, b, c, d): ###
     pass ###
+def update_controls():
+    pass ###
 
 class State(Enum):
     TITLE = 1
 
+#def update(delta_time):   # not yet supported in the engine
 def update():
-    pass ###
+    global state, game, accumulated_time, demo_reset_timer, demo_start_timer
+    delta_time = 0   ###
+
+    update_controls()
+
+    def button_pressed_controls(button_num):
+        for controls in (keyboard_controls,):
+            if controls is not None and controls.button_pressed(button_num):
+                return controls
+        return None
+
+    if state == State.TITLE:
+        controls = button_pressed_controls(0)
+        if controls is not None:
+            state = State.PLAY
+            game = Game(controls)
+
+        demo_reset_timer -= delta_time
+        demo_start_timer += delta_time
+        if demo_reset_timer <= 0:
+            game = Game()
+            demo_reset_timer = 60 * 2
+            demo_start_timer = 0
+
+    elif state == State.PLAY:
+        if game.race_complete:
+            state = State.GAME_OVER
+
+    elif state == State.GAME_OVER:
+        if button_pressed_controls(0) is not None:
+            game.player_car.stop_engine_sound()
+            state = State.TITLE
+            game = Game()
+            play_music("title_theme")
+
+    accumulated_time += delta_time
+    while accumulated_time >= FIXED_TIMESTEP:
+        accumulated_time -= FIXED_TIMESTEP
+        game.update(FIXED_TIMESTEP)
 
 def draw():
     game.draw()
@@ -43,8 +90,10 @@ def draw():
         screen.blit(logo_img, (WIDTH//2 - logo_img.get_width() // 2,
                                HEIGHT//3 - logo_img.get_height() // 2))
 
+keyboard_controls = KeyboardControls()
 state = State.TITLE
 game = Game()
 demo_reset_timer, demo_start_timer = 2 * 60, 0
+accumulated_time = 0
 
 run()
