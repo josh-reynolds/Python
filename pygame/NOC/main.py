5cd586b24453d6576d531983e6dde1ed735411f5
josh-reynolds <josh.reynolds@frontier.com>
2025-05-06 11:26:22 -0700

    Random walker biases to move to the right - NOC Example 1.3

--- pygame/NOC/main.py
+++ pygame/NOC/main.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, random
 from engine import *
 
 WIDTH = 640
@@ -16,28 +16,38 @@ class Walker:
         screen.draw.circle(self.x, self.y, 10, (0,0,0))
 
     def step(self):
-        step_x = randint(0,2) - 1
-        step_y = randint(0,2) - 1
-        self.x += step_x
-        self.y += step_y
+        #step_x = randint(0,2) - 1
+        #step_y = randint(0,2) - 1
+        #self.x += step_x
+        #self.y += step_y
+        r = random()
+        if (r < 0.4):
+            self.x += 1
+        elif (r < 0.6):
+            self.x -= 1
+        elif (r < 0.8):
+            self.y += 1
+        else:
+            self.y -= 1
+
 # ----------------------------------------------------
 
 # ----------------------------------------------------
 def update():
-    #w.step()
+    w.step()
 
     idx = randint(0, len(randomCounts)-1)
     randomCounts[idx] += 1
 
 def draw():
-    #w.display()
-
     bar_width = WIDTH // len(randomCounts)
     for i in range(len(randomCounts)):
         rect = (i * bar_width, HEIGHT - randomCounts[i], bar_width-1, randomCounts[i])
         screen.draw.rect(rect, (0,255,0), 0)
 
-#w = Walker()
+    w.display()
+
+w = Walker()
 
 randomCounts = [0 for i in range(20)]
 
