82300ac1ca2a590c5eeb9bb5196637444441b99e
josh-reynolds <josh.reynolds@frontier.com>
2025-05-07 20:11:20 -0700

    Adding a simple random value grid and cleaning up grid drawing code

--- pygame/noise/main.py
+++ pygame/noise/main.py
@@ -1,13 +1,22 @@
+from random import randint
 from engine import *
 
 WIDTH = 400
 HEIGHT = 400
 TITLE = "Perlin Noise"
 
+OUTLINE = (0,0,0)
+
 grid_w = 20
 grid_h = 20
 cell_w = WIDTH // grid_w
 cell_h = HEIGHT // grid_h
+
+def random_grayscale():
+    value = randint(0,255)
+    return (value, value, value)
+
+cells = [[random_grayscale() for i in range(grid_w)] for i in range(grid_h)]
 
 def update():
     pass
@@ -15,8 +24,10 @@ def update():
 def draw():
     for i in range(grid_w):
         for j in range(grid_h):
-            screen.draw.rect((i * cell_w, j * cell_h, cell_w, cell_h), (128,128,128), 0)
-            screen.draw.rect((i * cell_w, j * cell_h, cell_w, cell_h), (0,0,0), 1)
+            cell_rect = (i * cell_w, j * cell_h, cell_w, cell_h)
+
+            screen.draw.rect(cell_rect, cells[i][j], 0)
+            screen.draw.rect(cell_rect, OUTLINE, 1)
 
 
 
