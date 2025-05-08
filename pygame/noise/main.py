bddeb01f4bc198877ba32c5faaae5722317e3323
josh-reynolds <josh.reynolds@frontier.com>
2025-05-08 14:37:10 -0700

    Extracting noise calculation to a function

--- pygame/noise/main.py
+++ pygame/noise/main.py
@@ -15,7 +15,7 @@ cell_h = HEIGHT // grid_h
 cells = [[0 for i in range(grid_w)] for j in range(grid_h)]
 
 # ---------------------------------------
-scale = 0.09
+scale = 0.11
 seed(100000)
 #gradients = [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]
 gradients = [(1,1), (math.sqrt(2),0), (1,-1), (0,math.sqrt(2)), 
@@ -25,45 +25,46 @@ random_values = [[randint(0,7) for i in range(grid_w)] for i in range(grid_h)]
 min_val = 0
 max_val = 0
 
-def fade(t):
-    return t * t * t * (t * (t * 6 - 15) + 10)
+def noise(x_in, y_in):
+    x = x_in * scale
+    y = y_in * scale
+
+    # grid corners
+    x1, y1 = int(x), int(y)
+    x2, y2 = x1 + 1, y1 + 1
+
+    # distance vectors
+    dA, dB, dC, dD = (x1 - x, y1 - y), \
+                     (x1 - x, y2 - y), \
+                     (x2 - x, y1 - y), \
+                     (x2 - x, y2 - y)
+
+    # gradient vectors
+    gA, gB, gC, gD = gradients[random_values[x1][y1]], \
+                     gradients[random_values[x1][y2]], \
+                     gradients[random_values[x2][y1]], \
+                     gradients[random_values[x2][y2]]
+
+    # dot products
+    dotA, dotB, dotC, dotD = (dA[0] * gA[0] + dA[1] * gA[1]), \
+                             (dB[0] * gB[0] + dB[1] * gB[1]), \
+                             (dC[0] * gC[0] + dC[1] * gC[1]), \
+                             (dD[0] * gD[0] + dD[1] * gD[1])
+
+    def fade(t):
+        return t * t * t * (t * (t * 6 - 15) + 10)
+
+    # fade values
+    u, v = fade(x - x1), fade(y - y1)
+    
+    # interpolation
+    tmp1 = u * dotC + (1 - u) * dotA  # top edge
+    tmp2 = u * dotD + (1 - u) * dotB  # bottom edge
+    return v * tmp2 + (1 - v) * tmp1  # vertical
 
 for i in range(grid_w):
     for j in range(grid_h):
-        x = i * scale
-        y = j * scale
-
-        # grid corners
-        x1, y1 = int(x), int(y)
-        x2, y2 = x1 + 1, y1 + 1
-
-        # distance vectors
-        dA, dB, dC, dD = (x1 - x, y1 - y), \
-                         (x1 - x, y2 - y), \
-                         (x2 - x, y1 - y), \
-                         (x2 - x, y2 - y)
-
-        # gradient vectors
-        gA, gB, gC, gD = gradients[random_values[x1][y1]], \
-                         gradients[random_values[x1][y2]], \
-                         gradients[random_values[x2][y1]], \
-                         gradients[random_values[x2][y2]]
-
-        # dot products
-        dotA, dotB, dotC, dotD = (dA[0] * gA[0] + dA[1] * gA[1]), \
-                                 (dB[0] * gB[0] + dB[1] * gB[1]), \
-                                 (dC[0] * gC[0] + dC[1] * gC[1]), \
-                                 (dD[0] * gD[0] + dD[1] * gD[1])
-
-        # fade values
-        u, v = fade(x - x1), fade(y - y1)
-        
-        # interpolation
-        tmp1 = u * dotC + (1 - u) * dotA
-        tmp2 = u * dotD + (1 - u) * dotB
-        pixel_value = v * tmp2 + (1 - v) * tmp1
-
-        color_value = int(pixel_value * 255)
+        color_value = int(noise(i,j) * 255)
 
         if color_value < min_val:
             min_val = color_value
