ad99e84952ee22635cf8f0fc12489fa1c48e4c58
josh-reynolds <josh.reynolds@frontier.com>
2025-05-09 12:00:23 -0700

    Drawing hexagon vertices

--- pygame/hexes/main.py
+++ pygame/hexes/main.py
@@ -7,12 +7,14 @@ WIDTH = 400
 HEIGHT = 400
 TITLE = "Hexes"
 
+TWO_PI = math.pi * 2
 HALF_QUARTER = transformations.rotation_z(math.pi / 2)
+SIXTY_DEGREES = transformations.rotation_z(TWO_PI/6)
 points = []
 
-def rotate_around_center(point):
+def rotate_around_center(point, angle):
     p = transformations.translation(-WIDTH//2, -HEIGHT//2, 0) * point
-    p = HALF_QUARTER * p
+    p = angle * p
     return transformations.translation(WIDTH//2, HEIGHT//2 , 0) * p
 
 def update():
@@ -24,7 +26,14 @@ def draw():
         screen.draw.circle(p.x, p.y, 10, (0,0,0))
 
 points.append(tuples.point(WIDTH//2 + 30, HEIGHT//2, 0))
-for i in range(3):
-    points.append(rotate_around_center(points[i]))
+for i in range(5):
+    points.append(rotate_around_center(points[i], SIXTY_DEGREES))
 
 run()
+
+
+# vertices in a hexagon:
+#     for i in sides:
+#         angle = TWO_PI/sides * i
+#         vX = radius * cos(angle)
+#         vY = radius * sin(angle)
