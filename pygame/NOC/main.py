17b5c33213609765864e23669a52147f48f5d8fe
josh-reynolds <josh.reynolds@frontier.com>
2025-05-12 13:11:51 -0700

    Rudimentary bouncing ball - NOC Example 1.1

--- pygame/NOC/main.py
+++ pygame/NOC/main.py
@@ -1,19 +1,38 @@
 from engine import *
-from walker import Walker
 
 WIDTH = 640
-HEIGHT = 240
+HEIGHT = 360
 TITLE ="The Nature of Code"
+
+class Ball:
+    def __init__(self, x, y, xspeed, yspeed):
+        self.x = x
+        self.y = y
+        self.xspeed = xspeed
+        self.yspeed = yspeed
+
+    def update(self):
+        self.x += self.xspeed
+        self.y += self.yspeed
+
+        if ((self.x > WIDTH) or (self.x < 0)):
+            self.xspeed *= -1
+        if ((self.y > HEIGHT) or (self.y < 0)):
+            self.yspeed *= -1
+
+    def draw(self):
+        screen.draw.circle(self.x, self.y, 10, (255,0,0))
 
 # ----------------------------------------------------
 def update():
-    w.step()
+    b.update()
 
 # ----------------------------------------------------
 def draw():
-    w.display(screen)
+    b.draw()
 
-w = Walker(WIDTH//2, HEIGHT//2)
+# NOC Example 1.1 (p. 29) ----------------------------
+b = Ball(100,100, 1, 3.3)
 
 run()
 
@@ -30,3 +49,12 @@ run()
 # Latest hurdle: Processing includes a noise() function to generate Perlin noise. I don't
 # have the equivalent in Pygame or stock Python. I _could_ import a module, but I am
 # contemplating rolling my own and sticking into the engine. Researching now.
+
+# Perlin noise project is working, need to package it up and add to the engine...
+
+# Moving ahead to Chapter 1 now. New issue: the engine can't access top-level primitive 
+# variables - or at least it can't modify them. But if we wrap the values in a class, it
+# works fine. I think this is a referencing problem due to the way the engine finds the
+# content of this parent script, need to fiddlw with this to sort it out. But for now,
+# going ahead with class-based solution, which is why Example 1.1 above looks a little
+# different from the book version.
