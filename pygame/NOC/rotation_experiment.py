dd3f51594ac11d8220d2472b435bcd78a2ae3f7e
josh-reynolds <josh.reynolds@frontier.com>
2025-05-19 11:17:11 -0700

    Sorting out correct way to apply alpha channel to surface - this was causing the issues previously

--- pygame/NOC/rotation_experiment.py
+++ pygame/NOC/rotation_experiment.py
@@ -4,6 +4,13 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((400,400))
 running = True
+
+
+s = pygame.Surface((80,20), flags=SRCALPHA)
+#s.set_alpha(255)      # Ah! This is the issue!
+s.fill((255,0,0))
+s = pygame.transform.rotate(s, 15)
+
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -13,6 +20,7 @@ while running:
                 running = False
 
     screen.fill((0,0,255))
+    screen.blit(s, (200-40,200-10))
     pygame.display.update()
 
 pygame.quit()
