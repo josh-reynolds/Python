405b4bdf17d5795afb0ded5c9836b70cb8690cbb
josh-reynolds <josh.reynolds@frontier.com>
2025-05-24 14:17:09 -0700

    Swinging pendulum - NOC Example 3.10

--- pygame/NOC/main.py
+++ pygame/NOC/main.py
@@ -17,17 +17,40 @@ WIDTH = 640
 HEIGHT = 360
 TITLE = "The Nature of Code"
 
+class Pendulum:
+    def __init__(self, origin, r):
+        self.origin = origin
+        self.r = r
+        self.angle = math.pi/4
+        self.a_velocity = 0
+        self.a_accel = 0
+        self.damping = 0.995
+
+    def update(self):
+        gravity = 0.4
+        self.a_accel = (-1 * gravity/self.r) * math.sin(self.angle)
+        self.a_velocity += self.a_accel
+        self.angle += self.a_velocity
+        self.a_velocity *= self.damping
+
+    def draw(self):
+        location = PVector(self.r * math.sin(self.angle),
+                           self.r * math.cos(self.angle))
+        location + self.origin
+        screen.draw.line((0,0,0), (self.origin.x, self.origin.y), (location.x, location.y))
+        screen.draw.circle(location.x, location.y, 16, (0,0,255))
+
 # ----------------------------------------------------
 def update():
-    pass
+    p.update()
 # ----------------------------------------------------
 
 # ----------------------------------------------------
 def draw():
-    w.draw()
+    p.draw()
 # ----------------------------------------------------
 
-w = Wave(0, WIDTH//2, 5, 0.1, HEIGHT)
+p = Pendulum(PVector(WIDTH//2,10), 125)
 
 # ----------------------------------------------------
 run()
