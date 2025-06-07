b8558a2ca01398f4ca8e1d889189864469723a32
josh-reynolds <josh.reynolds@frontier.com>
2025-06-07 11:03:47 -0700

    Still more words, and cleaning up redundant comments

--- RPGs/adventure_titles.py
+++ RPGs/adventure_titles.py
@@ -1,60 +1,21 @@
 from random import choice
 
-# The Black Castle
-# City of Reavers
-# Field of Blood
-# Tomb of Snakes
-# Island of Wonders
-# City of Kings
-# Island of Obelisks
-# Plain of Bones
-# Tomb of Horrors
-# The Forgotten Tomb
-# Keep on the Borderlands
-# The Sunless Citadel
-# The Tower of the Witch
-# The Lost Caverns
-# The Forgotten Temple
-# Shrine of the ...
-# Vault of the ...
-# Fortress of the ...
-# The Forbidden City
-# Pit of Night
-# The Ebon Door
-
-# Blood, Bone, Ash, Ebon, Night, Dark, Lost, Forgotten, Black, Snakes, Reavers, Wonders, Kings,
-# Obelisks, Horrors, Sunless, Iron, Sinister, Secrets, Winter, Ice, Frost, Evil, Chaos, Forsaken,
-# Burning, Damned, Buried, Stone, Ruined, Collapsed, Dread, 
-
-
-# Castle, City, Field, Tomb, Crypt, Island, Plain, Keep, Citadel, Tower, Ruin, Caverns, Shrine,
-# Vault, Fortress, Pit, Door, Bog, Swamp, Chasm, Lake, Tarn, Tor, Village, Temple, Gate, Fountain,
-# Secret, Mystery, Barrow, Burial Mound, Lair, Watchtower, Eyrie, Sepulcher, Catacombs, 
-
-# Witch, Snake, Demon, Dragon, Beast, Warlord, King, Conqueror, Spiders, Frogs, Wolves, Bear,
-# Warlock, Wizard, Dwarf, Elf, 
-
-# The <Adjective> <Location>
-# <Location> of <Group>
-# <Location> of <Thing>
-# <Location> on the <Landscape>
-# The <Location> of the <Individual>
-# <Location> of the <Group>
-
 def adjectives():
     return choice(["bloody", "ashen", "ebon", "dark", "lost", "forgotten", "black", "red", "sunless", 
                    "iron", "sinister", "icy", "evil", "chaotic", "forsaken", "burning", "damned", 
                    "buried", "stone", "ruined", "collapsed", "dread", "secret", "hellish",
                    "putrid", "rotten", "decayed", "white", "shadowed", "festering", "frozen",
                    "weird", "blue", "green", "yellow", "orange", "purple", "golden", "silver",
-                   "scarred"])
+                   "scarred", "charred", "hidden", "mysterious", "mad", "slave", "subterranean",
+                   "fey", "glowing", "sunken"])
 
 def locations():
     return choice(["castle", "city", "field", "tomb", "crypt", "island", "plain", "keep", "citadel", 
                    "tower", "ruin", "caverns", "shrine", "vault", "fortress", "pit", "door", 
                    "bog", "swamp", "chasm", "lake", "tarn", "tor", "village", "temple", "gate", 
                    "fountain", "barrow", "burial" "mound", "lair", "watchtower", "eyrie", 
-                   "sepulcher", "catacombs", "fort", "isle", "cave"])
+                   "sepulcher", "catacombs", "fort", "isle", "cave", "well", "pyramid", "ziggurat",
+                   "hall", "cathedral", "library"])
 
 def groups():
     return choice(["witches", "snakes", "demons", "dragons", "beasts", "warlords", "kings", 
@@ -67,13 +28,16 @@ def groups():
                    "hounds", "imps", "jackals", "werewolves", "jaguars", "leopards", "lions", "tigers",
                    "lynx", "mammoths", "elephants", "manticores", "hags", "nightmares", "octopuses",
                    "squids", "orcs", "otters", "owls", "worms", "rats", "salamanders", "sharks",
-                   "shadows", "sphinxes", "stags", "unicorns", "troglodytes", "toads", "wasps", "wyverns"])
+                   "shadows", "sphinxes", "stags", "unicorns", "troglodytes", "toads", "wasps", "wyverns",
+                   "flies", "maggots", "angels", "thieves", "assassins", "druids", "bards"])
 
 def things():
     return choice(["bone", "ash", "blood", "night", "snake", "reaver", "wonder", "obelisk", 
                    "horror", "secret", "ice", "frost", "stone", "hell", "mold", "rot", "slime",
                    "filth", "sword", "wand", "crown", "tombstone", "throne", "staff", "helm",
-                   "bile", "flesh", "soot", "spear", "evil", "chaos", "skull"])
+                   "bile", "flesh", "soot", "spear", "evil", "chaos", "skull", "souls", "spirits",
+                   "fire", "axe", "madness", "orb", "tome", "codex", "grimoire", "cauldron",
+                   "victory", "hope"])
 
 def landscapes():
     return choice(["borderlands", "hill", "plain", "forest", "swamp", "bog", "canyon", "lake",
@@ -90,7 +54,8 @@ def individuals():
                    "hound", "imp", "jackal", "werewolf", "jaguar", "leopard", "lion", "tiger",
                    "lynx", "mammoth", "elephant", "manticore", "hag", "nightmare", "octopus",
                    "squid", "orc", "otter", "owl", "worm", "rat", "salamander", "shark",
-                   "shadow", "sphinx", "stag", "unicorn", "troglodyte", "toad", "wasp", "wyvern"])
+                   "shadow", "sphinx", "stag", "unicorn", "troglodyte", "toad", "wasp", "wyvern",
+                   "fly", "maggot", "angel", "thief", "assassin", "druid", "bard"])
 
 def make_title():
     return choice([
@@ -100,7 +65,8 @@ def make_title():
         f"{locations()} on the {landscapes()}",
         f"The {locations()} of the {individuals()}",
         f"{locations()} of the {groups()}",
-        f"The {locations()} of {adjectives()} {things()}"])
+        f"The {locations()} of {adjectives()} {things()}",
+        f"The {things()} of {adjectives()} {locations()}"])
 
 
 if __name__ == '__main__':
