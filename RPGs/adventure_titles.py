5838944d6089842c558598151a8be753c83391c6
josh-reynolds <josh.reynolds@frontier.com>
2025-06-06 20:48:30 -0700

    Adding more words, and imposing title case on the output

--- RPGs/adventure_titles.py
+++ RPGs/adventure_titles.py
@@ -44,30 +44,53 @@ from random import choice
 def adjectives():
     return choice(["bloody", "ashen", "ebon", "dark", "lost", "forgotten", "black", "red", "sunless", 
                    "iron", "sinister", "icy", "evil", "chaotic", "forsaken", "burning", "damned", 
-                   "buried", "stone", "ruined", "collapsed", "dread", "secret"])
+                   "buried", "stone", "ruined", "collapsed", "dread", "secret", "hellish",
+                   "putrid", "rotten", "decayed", "white", "shadowed", "festering", "frozen",
+                   "weird", "blue", "green", "yellow", "orange", "purple", "golden", "silver",
+                   "scarred"])
 
 def locations():
     return choice(["castle", "city", "field", "tomb", "crypt", "island", "plain", "keep", "citadel", 
                    "tower", "ruin", "caverns", "shrine", "vault", "fortress", "pit", "door", 
                    "bog", "swamp", "chasm", "lake", "tarn", "tor", "village", "temple", "gate", 
                    "fountain", "barrow", "burial" "mound", "lair", "watchtower", "eyrie", 
-                   "sepulcher", "catacombs"])
+                   "sepulcher", "catacombs", "fort", "isle", "cave"])
 
 def groups():
     return choice(["witches", "snakes", "demons", "dragons", "beasts", "warlords", "kings", 
                    "conquerors", "spiders", "frogs", "wolves", "bears", "warlocks", "wizards", 
-                   "dwarves", "elves", "reavers", "wonders", "obelisks", "horrors"])
+                   "dwarves", "elves", "reavers", "wonders", "obelisks", "horrors", "giants",
+                   "ogres", "trolls", "vampires", "bats" , "skeletons", "zombies", "goblins",
+                   "wraiths", "phantoms", "ghosts", "ghouls", "griffons", "apes", "beetles", "boars",
+                   "crabs", "scorpions", "bulls", "centaurs", "minotaurs", "devils", "fiends", "djinns",
+                   "turtles", "lizards", "eagles", "falcons", "hawks", "eels", "fungi", "gargoyles",
+                   "hounds", "imps", "jackals", "werewolves", "jaguars", "leopards", "lions", "tigers",
+                   "lynx", "mammoths", "elephants", "manticores", "hags", "nightmares", "octopuses",
+                   "squids", "orcs", "otters", "owls", "worms", "rats", "salamanders", "sharks",
+                   "shadows", "sphinxes", "stags", "unicorns", "troglodytes", "toads", "wasps", "wyverns"])
 
 def things():
     return choice(["bone", "ash", "blood", "night", "snake", "reaver", "wonder", "obelisk", 
-                   "horror", "secret", "ice", "frost", "stone"])
+                   "horror", "secret", "ice", "frost", "stone", "hell", "mold", "rot", "slime",
+                   "filth", "sword", "wand", "crown", "tombstone", "throne", "staff", "helm",
+                   "bile", "flesh", "soot", "spear", "evil", "chaos", "skull"])
 
 def landscapes():
-    return choice( ["borderlands", "hill", "plain", "forest", "swamp", "bog", "canyon", "lake"])
+    return choice(["borderlands", "hill", "plain", "forest", "swamp", "bog", "canyon", "lake",
+                   "pond", "mountain", "peak", "volcano", "caldera", "rift", "chasm", "marsh",
+                   "river"])
 
 def individuals():
     return choice(["witch", "snake", "demon", "dragon", "beast", "warlord", "king", "conqueror", 
-                   "spider", "frog", "wolf", "bear", "warlock", "wizard", "dwarf", "elf"])
+                   "spider", "frog", "wolf", "bear", "warlock", "wizard", "dwarf", "elf", "giant",
+                   "ogre", "troll", "lich", "vampire", "bat", "skeleton", "zombie", "goblin",
+                   "wraith", "phantom", "ghost", "ghoul", "griffon", "ape", "beetle", "boar",
+                   "crab", "scorpion", "bull", "centaur", "minotaur", "devil", "fiend", "djinn",
+                   "turtle", "lizard", "eagle", "falcon", "hawk", "eel", "fungus", "gargoyle",
+                   "hound", "imp", "jackal", "werewolf", "jaguar", "leopard", "lion", "tiger",
+                   "lynx", "mammoth", "elephant", "manticore", "hag", "nightmare", "octopus",
+                   "squid", "orc", "otter", "owl", "worm", "rat", "salamander", "shark",
+                   "shadow", "sphinx", "stag", "unicorn", "troglodyte", "toad", "wasp", "wyvern"])
 
 def make_title():
     return choice([
@@ -76,10 +99,11 @@ def make_title():
         f"{locations()} of {things()}",
         f"{locations()} on the {landscapes()}",
         f"The {locations()} of the {individuals()}",
-        f"{locations()} of the {groups()}"])
+        f"{locations()} of the {groups()}",
+        f"The {locations()} of {adjectives()} {things()}"])
 
 
 if __name__ == '__main__':
     for i in range(30):
-        print(make_title())
+        print(make_title().title())
 
