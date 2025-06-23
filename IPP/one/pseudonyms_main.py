376ac50ea5860e99aa11c064286cf682c19d4366
josh-reynolds <josh.reynolds@frontier.com>
2025-06-23 13:04:46 -0700

    Starting pylint review of script, adding standard __name__ == __main__ approach

---
+++ IPP/one/pseudonyms_main.py
@@ -0,0 +1,50 @@
+from random import choice
+
+# function not present in the book project - see note below
+def pr_red(string):
+    print(f"\033[91m {string}\033[00m")
+
+def main():
+    print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+    print("A name just like Sean would pick for Gus:\n\n")
+
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+             "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+             'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+             'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+             'Crapp', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+             'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+             'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"',)
+
+
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom', 'Breedslovetrout',
+            'Butterbaugh', 'Clovenhoof', 'Clutterbuck', 'Cocktoasten', 'Endicott', 'Fewhairs',
+            'Gooberdapple', 'Goodensmith', 'Goodpasture', 'Guster', 'Henderson', 'Hooperbag',
+            'Hoosenater', 'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson')
+
+
+    while True:
+        first_name = choice(first)
+        last_name = choice(last)
+
+        print("\n\n")
+        #print(f"{first_name} {last_name}", file=sys.stderr)
+        pr_red(f"{first_name} {last_name}")
+        print("\n\n")
+
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+        if try_again.lower() == 'n':
+            break
+
+    input("\nPress Enter to exit.")
+
+if __name__ == "__main__":
+    main()
+
+# The book project uses sys.stderr to color the output red, but this assumes
+# the script is being run in IDLE. In my console, this does not provide any coloring
+# (and obfuscates things by appearing as an error).
+#
+# There are some modules that wrap ANSI codes for this, but it's also possible to
+# do it raw, so going to try that out.
