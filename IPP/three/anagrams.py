949186a5825d58f24d18a16040e878939ae04f47
josh-reynolds <josh.reynolds@frontier.com>
2025-06-25 11:54:35 -0700

    First version of anagram script

---
+++ IPP/three/anagrams.py
@@ -0,0 +1,23 @@
+import load_dictionary
+
+word_list = load_dictionary.load("words.txt")
+
+anagram_list = []
+
+name = 'Foster'
+print(f"Input name = {name}")
+name = name.lower()
+print(f"Using name = {name}")
+
+name_sorted = sorted(name)
+for word in word_list:
+    word = word.lower()
+    if word != name:
+        if sorted(word) == name_sorted:
+            anagram_list.append(word)
+
+print()
+if len(anagram_list) == 0:
+    print("You need a larger dictionary or a new name!")
+else:
+    print("Anagrams =", *anagram_list, sep='\n')
