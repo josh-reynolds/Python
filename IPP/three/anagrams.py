8762d0693e1e714941961599fd83e6086c31fcb5
josh-reynolds <josh.reynolds@frontier.com>
2025-06-25 12:29:14 -0700

    Adding find_anagrams() function

--- IPP/three/anagrams.py
+++ IPP/three/anagrams.py
@@ -1,18 +1,43 @@
 """Find all anagrams in a dictionary file for a given word."""
+import sys
 from collections import Counter
 import load_dictionary
 
-word_list = load_dictionary.load("words.txt")
+dict_file = load_dictionary.load("words.txt")
+dict_file.append('a')
+dict_file.append('I')
+dict_file = sorted(dict_file)
+
+ini_name = input("Enter a name: ")
+
+def find_angrams(name, word_list):
+    """Read name & dictionary file and display all anagrams IN name."""
+    name_letter_map = Counter(name)
+    anagrams = []
+    for word in word_list:
+        test = ''
+        word_letter_map = Counter(word.lower())
+        for letter in word:
+            if word_letter_map[letter] <= name_letter_map[letter]:
+                test += letter
+        if Counter(test) == word_letter_map:
+            anagrams.append(word)
+    print(*anagrams, sep='\n')
+    print()
+    print(f"Remaining letters = {name}")
+    print(f"Number of remaining letters = {len(name)}")
+    print(f"Number of remaining (real word) anagrams = {len(anagrams)}")
+
+
 anagram_list = []
 
 while True:
-    name = input("Input a name.\n")
-    print(f"Input name = {name}")
-    name = name.lower()
+    print(f"Input name = {ini_name}")
+    name = ini_name.lower()
     print(f"Using name = {name}")
 
     name_count = Counter(name)
-    for word in word_list:
+    for word in dict_file:
         word_count = Counter(word.lower())
         if word != name:
             if word_count == name_count:
