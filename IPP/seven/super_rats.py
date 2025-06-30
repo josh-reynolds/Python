68f49fd8e44a4500c82627f5e5b518bb7be8e9d2
josh-reynolds <josh.reynolds@frontier.com>
2025-06-30 11:33:42 -0700

    Adding fitness(), select() and breed() functions

--- IPP/seven/super_rats.py
+++ IPP/seven/super_rats.py
@@ -21,3 +21,31 @@ def populate(num_rats, min_wt, mx_wt, mode_wt):
     """Initialize a population with a triangular distribution of weights."""
     return [int(random.triangular(min_wt, max_wt, mode_wt))
             for i in range(num_rats)]
+
+def fitness(population, goal):
+    """Measure population fitness based on an attribute mean vs target."""
+    ave = statistics.mean(population)
+    return ave / goal
+
+def select(population, to_retain):
+    """Cull a population to retain only a specified number of members."""
+    sorted_population = sorted(population)
+    to_retain_by_sex = to_retain//2
+    members_per_sex = len(sorted_population)//2
+    females = sorted_population[:members_per_sex]
+    males = sorted_population[members_per_sex:]
+    selected_females = females[-to_retain_by_sex:]
+    selected_males = males[-to_retain_by_sex:]
+    return selected_males, selected_females
+
+def breed(males, females, litter_size):
+    """Crossover genes among members (weights) of a population."""
+    random.shuffle(males)
+    random.shuffle(females)
+    children = []
+    for male, female in zip(males, females):
+        for child in range(litter_size):
+            child = random.randint(female, male)
+            children.append(child)
+    return children
+
