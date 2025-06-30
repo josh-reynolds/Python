f3aadb03c95675a413f3c510b9f1b74c3b6496ff
josh-reynolds <josh.reynolds@frontier.com>
2025-06-30 10:04:06 -0700

    Finishing the invisible ink script

--- IPP/six/elementary_ink.py
+++ IPP/six/elementary_ink.py
@@ -20,3 +20,29 @@ subtitle.alignment = 1
 doc.add_heading('', 1)
 doc.add_paragraph('December 17, 2015')
 doc.add_paragraph('')
+
+def set_spacing(paragraph):
+    """Use docx to set line spacing between paragraphs."""
+    paragraph_format = paragraph.paragraph_format
+    paragraph_format.space_before = Pt(0)
+    paragraph_format.space_after = Pt(0)
+
+length_real = len(real_list)
+count_real = 0    # index of current line in real (hidden) message
+
+for line in fake_list:
+    if count_real < length_real and line == "":
+        paragraph = doc.add_paragraph(real_list[count_real])
+        paragraph_index = len(doc.paragraphs) - 1
+        run = doc.paragraphs[paragraph_index].runs[0]
+        font = run.font
+        font.color.rgb = RGBColor(255,255,255)
+        count_real += 1
+    else:
+        paragraph = doc.add_paragraph(line)
+
+    set_spacing(paragraph)
+
+doc.save('ciphertext_message_letterhead.docx')
+
+print("Done")
