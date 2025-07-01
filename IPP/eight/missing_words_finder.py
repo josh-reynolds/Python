a078a5232291b48679658ed06055d421d21d5844
josh-reynolds <josh.reynolds@frontier.com>
2025-07-01 12:44:26 -0700

    Starting skeleton for missing_words_finder script

---
+++ IPP/eight/missing_words_finder.py
@@ -0,0 +1,34 @@
+"""Review CMUDICT for missing words."""
+import sys
+from string import punctuation
+import pprint
+import json
+from nltk.corpus import cmudict
+
+cmudict = cmudict.dict()   # Carnegie Mellon University Pronouncing Dictionary
+
+def main():
+    """Examine CMUDICT for missing words and build exception list."""
+    haiku = load_haiku('train.txt')
+    exceptions = cmudict_missing(haiku)
+    build_dict = input("\nManually build an exceptions dictionary (y/n)?\n")
+    if build_dict.lower() == 'n':
+        sys.exit()
+    else:
+        missing_words_dict = make_exceptions_dict(exceptions)
+        save_exceptions(missing_words_dict)
+
+def load_haiku(file):
+    """Open and return training corpus of haiku as a set."""
+
+def cmudict_missing(words):
+    """Find and return words in word set missing from cmudict."""
+
+def make_exceptions_dict(words):
+    """Return dictionary of words and syllable counts from a set of words."""
+
+def save_exceptions(dictionary):
+    """Save exceptions dictionary as a json file."""
+
+if __name__ == "__main__":
+    main()
