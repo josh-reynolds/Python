2965d72523ff1001613715c886865817180d6656
josh-reynolds <josh.reynolds@frontier.com>
2025-07-02 13:03:45 -0700

    Filling in make_exceptions_dict() stub

--- IPP/eight/missing_words_finder.py
+++ IPP/eight/missing_words_finder.py
@@ -42,11 +42,53 @@ def cmudict_missing(word_set):
     print(f"cmudict membership = {membership:0.1f}{'%'}")
     return exceptions
 
-def make_exceptions_dict(words):
+def make_exceptions_dict(exceptions_set):
     """Return dictionary of words and syllable counts from a set of words."""
+    missing_words = {}
+    print("Input # syllables in word. Mistakes can be corrected at end. \n")
+    for word in exceptions_set:
+        while True:
+            num_sylls = input(f"Enter number syllables in {word}: ")
+            if num_sylls.isdigit():
+                break
+            else:
+                pr_red("            Not a valid answer!")
+        missing_words[word] = int(num_sylls)
+    print()
+    pprint.pprint(missing_words, width=1)
+
+    print("\nMake Changes to Dictionary Before Saving?")
+    print("""
+    0 - Exit and Save
+    1 - Add a Word or Change a Syllable Count
+    2 - Remove a Word
+    """)
+
+    while True:
+        choice = input("\nEnter choice: ")
+        if choice == '0':
+            break
+        elif choice == '1':
+            word = input("\nWord to add or change: ")
+            missing_words[word] = int(input(f"Enter number "
+                                            f"syllables in {word}:"))
+        elif choice == '2':
+            word = input("\nEnter word to delete: ")
+            missing_words.pop(word, None)
+
+    print("\nNew words or syllable changes:")
+    pprint.pprint(missing_words, width=1)
+
+    return missing_words
+
+
 
 def save_exceptions(dictionary):
     """Save exceptions dictionary as a json file."""
+
+def pr_red(output):
+    """Print string to console, colored red."""
+    print(f"\033[91m{output}\033[00m")
 
 if __name__ == "__main__":
     main()
