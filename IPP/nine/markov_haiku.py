8fb31f5b078269a44dafd73c9eea9e75e8d71ef8
josh-reynolds <josh.reynolds@frontier.com>
2025-07-03 12:53:31 -0700

    Adding map_word_to_word() function

--- IPP/nine/markov_haiku.py
+++ IPP/nine/markov_haiku.py
@@ -2,11 +2,11 @@
 #import sys
 import logging
 #import random
-#from collections import defaultdict
+from collections import defaultdict
 #from count_syllables import count_syllables
 
 #logging.disable(logging.CRITICAL)
-logging.basicConfig(level=logging.DEBUG, format='%(messages)s')
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 def load_training_file(file):
     """Return text file as a string."""
@@ -19,10 +19,21 @@ def prep_training(raw_haiku):
     corpus = raw_haiku.replace('\n', ' ').split()
     return corpus
 
+def map_word_to_word(corpus):
+    """Load list and use dictionary to map word to word that follows."""
+    limit = len(corpus) - 1
+    dict1_to_1 = defaultdict(list)
+    for index, word in enumerate(corpus):
+        if index < limit:
+            suffix = corpus[index + 1]
+            dict1_to_1[word].append(suffix)
+    logging.debug('map_word_to_word results for \"sake\" = %s\n',
+                  dict1_to_1['sake'])
+    return dict1_to_1
+
 def main():
     """Generate haiku using a Markov chain."""
-    output = prep_training(load_training_file('train.txt'))
-    logging.debug(*output)
+    map_word_to_word(prep_training(load_training_file('train.txt')))
 
 if __name__ == '__main__':
     main()
