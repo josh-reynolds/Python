f98f25079822f36c2970da73a57172b8a334a4c5
josh-reynolds <josh.reynolds@frontier.com>
2025-07-05 12:52:20 -0700

    Starting setup for galaxy_simulator.py

---
+++ IPP/ten/galaxy_simulator.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from random import randint, uniform, random
+import math
+
+# ============================================================================
+# MAIN INPUT
+
+# scale (radio bubble diameter) in light-years:
+SCALE = 225     # enter 225 to see Earth's radio bubble
+
+# number of advanced civilizations from the Drake equation:
+NUM_CIVS = 15600000
+# ============================================================================
+
+root = tk.Tk()
+root.title("Milky Way galaxy")
+c = tk.Canvas(root, width=1000, height=800, bg='black')
+c.grid()
+c.configure(scrollregion=(-500, -400, 500, 400))
+
+# actual Milky Way dimensions (light-years)
+DISC_RADIUS = 50000
+DISC_HEIGHT = 1000
+DISC_VOL = math.pi * DISC_RADIUS**2 * DISC_HEIGHT
+
+def scale_galaxy():
+    """Scale galaxy dimensions based on radio bubble size (scale)."""
+    disc_radius_scaled = round(DISC_RADIUS / SCALE)
+    bubble_vol = 4/3 * math.pi * (SCALE / 2)**3
+    disc_vol_scaled = DISC_VOL / bubble_vol
+    return disc_radius_scaled, disc_vol_scaled
+
+def detect_prob(disc_vol_scaled):
+    """Calculate probability of galactic civilizations detecting each other."""
+    ratio = NUM_CIVS / disc_vol_scaled
+    if ratio < 0.002:
+        detection_prob = 0
+    elif ratio >= 5:
+        detection_prob = 1
+    else:
+        detection_prob = (-0.004757 * ratio**4 + 0.06681 * ratio**3 -
+                          0.3605 * ratio**2 + 0.9215 * ratio + 0.00826)
+    return round(detection_prob, 3)
