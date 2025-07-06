23c166bc7d79fa6a44196fe828246859fbead3f9
josh-reynolds <josh.reynolds@frontier.com>
2025-07-06 14:41:12 -0700

    Adding main() function and completing galaxy_simulator script

--- IPP/ten/galaxy_simulator.py
+++ IPP/ten/galaxy_simulator.py
@@ -66,9 +66,9 @@ def spirals(b, r, rot_fac, fuz_fac, arm):
         theta = math.radians(i)
         x = (r * math.exp(b * theta) * math.cos(theta + math.pi * rot_fac) +
              randint(-fuzz,fuzz) * fuz_fac)
-        l = (r * math.exp(b * theta) * math.sin(theta + math.pi * rot_fac) +
+        y = (r * math.exp(b * theta) * math.sin(theta + math.pi * rot_fac) +
              randint(-fuzz,fuzz) * fuz_fac)
-        spiral_Stars.append((x,y))
+        spiral_stars.append((x,y))
     for x,y in spiral_stars:
         if arm == 0 and int(x % 2) == 0:
             c.create_oval(x-2, y-2, x+2, y+2, fill='white', outline='')
@@ -86,6 +86,42 @@ def star_haze(disc_radius_scaled, density):
     for i in range(0, disc_radius_scaled * density):
         x,y = random_polar_coordinates(disc_radius_scaled)
         c.create_text(x, y, fill='white', font=('Helvetica', '7'), text='.')
+
+def main():
+    """Calculate detection probability & post galaxy display & statistics."""
+    disc_radius_scaled, disc_vol_scaled = scale_galaxy()
+    detection_prob = detect_prob(disc_vol_scaled)
+
+    # build 4 main spiral arms and 4 trailing arms
+    spirals(b=-0.3, r=disc_radius_scaled, rot_fac=2, fuz_fac=1.5, arm=0)
+    spirals(b=-0.3, r=disc_radius_scaled, rot_fac=1.91, fuz_fac=1.5, arm=1)
+    spirals(b=-0.3, r=-disc_radius_scaled, rot_fac=2, fuz_fac=1.5, arm=0)
+    spirals(b=-0.3, r=-disc_radius_scaled, rot_fac=-2.09, fuz_fac=1.5, arm=1)
+    spirals(b=-0.3, r=-disc_radius_scaled, rot_fac=0.5, fuz_fac=1.5, arm=0)
+    spirals(b=-0.3, r=-disc_radius_scaled, rot_fac=0.4, fuz_fac=1.5, arm=1)
+    spirals(b=-0.3, r=-disc_radius_scaled, rot_fac=-0.5, fuz_fac=1.5, arm=0)
+    spirals(b=-0.3, r=-disc_radius_scaled, rot_fac=-0.6, fuz_fac=1.5, arm=1)
+    star_haze(disc_radius_scaled, density=8)
+
+    # display legend
+    c.create_text(-455, -360, fill='white', anchor='w',
+                  text=f"One Pixel = {SCALE} LY")
+    c.create_text(-455, -330, fill='white', anchor='w',
+                  text=f"Radio Bubble Diameter = {SCALE} LY")
+    c.create_text(-455, -300, fill='white', anchor='w',
+                  text=f"Probability of detection for {NUM_CIVS:,} civilizations = {detection_prob}")
+
+    # post Earth's 225 LY diameter bubble and annotate
+    if SCALE == 225:
+        c.create_rectangle(115, 75, 116, 76, fill='red', outline='')
+        c.create_text(118, 72, fill='red', anchor='w',
+                      text="<----------- Earth's Radio Bubble")
+
+    # run tkinter loop
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
 
 
 
