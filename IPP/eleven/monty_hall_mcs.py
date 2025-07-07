4f64315145339d3fe278a274405281a41e7379ad
josh-reynolds <josh.reynolds@frontier.com>
2025-07-07 12:00:43 -0700

    Applying pylint/pydocstyle feedback to monty_hall_mcs script

--- IPP/eleven/monty_hall_mcs.py
+++ IPP/eleven/monty_hall_mcs.py
@@ -1,4 +1,6 @@
+"""Run Monte Carlo simulation of Monty Hall door game."""
 import random
+# pylint: disable=C0103
 
 def user_prompt(prompt, default=None):
     """Allow use of default values in input."""
@@ -6,8 +8,7 @@ def user_prompt(prompt, default=None):
     response = input(prompt)
     if not response and default:
         return default
-    else:
-        return response
+    return response
 
 num_runs = int(user_prompt("Input number of runs", "20000"))
 
@@ -26,9 +27,9 @@ for i in range(num_runs):
 
 print(f"Wins with original pick = {first_choice_wins}")
 print(f"Wins with changed pick = {pick_change_wins}")
-print(f"Probability of winning with initial guess: {first_choice_wins/num_runs:.2f}")
-print(f"Probability of winning by switching: {pick_change_wins/num_runs:.2f}")
+print(f"Probability of winning with initial "
+      f"guess: {first_choice_wins/num_runs:.2f}")
+print(f"Probability of winning by switching: "
+      f"{pick_change_wins/num_runs:.2f}")
 
-input("\Press Enter key to exit.")
-
-
+input("\nPress Enter key to exit.")
