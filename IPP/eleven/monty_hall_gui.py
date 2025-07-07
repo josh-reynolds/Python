080c71b757bcc0e82407b5fc30cfe89192d54e36
josh-reynolds <josh.reynolds@frontier.com>
2025-07-07 12:18:10 -0700

    Adding Game.create_widgets() method

--- IPP/eleven/monty_hall_gui.py
+++ IPP/eleven/monty_hall_gui.py
@@ -18,4 +18,22 @@ class Game(tk.Frame):
         self.pick_change_wins = 0
         self.create_widgets()
 
+    def create_widgets(self):
+        """Create label, button, and text widgets for game."""
+        img = tk.PhotoImage(file='all_closed.png')
+        self.photo_lbl = tk.Label(self.parent, image=img,
+                                  text='', borderwidth=0)
+        self.photo_lbl.grid(row=0, column=0, columnspan=10, sticky='W')
+        self.photo_lbl.image = img
+
+        instr_input = [
+                ('Behind one door is CASH!', 1, 0, 5, 'W'),
+                ('Behind the others: GOATS!!!', 2, 0, 5, 'W'),
+                ('Pick a door:', 1, 3, 1, 'E')
+                ]
+        for text, row, column, columnspan, sticky in instr_input:
+            instr_lbl = tk.Label(self.parent, text=text)
+            instr_lbl.grid(row=row, column=column, columnspan=columnspan,
+                           sticky=sticky,ipadx=30)
+
 
