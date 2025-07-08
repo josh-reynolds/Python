2eb82ccb939898925676c41ace353cd2466ba4f9
josh-reynolds <josh.reynolds@frontier.com>
2025-07-08 09:26:11 -0700

    Adding radio buttons to Game.create_widgets()

--- IPP/eleven/monty_hall_gui.py
+++ IPP/eleven/monty_hall_gui.py
@@ -36,4 +36,38 @@ class Game(tk.Frame):
             instr_lbl.grid(row=row, column=column, columnspan=columnspan,
                            sticky=sticky,ipadx=30)
 
+        self.door_choice = tk.StringVar()
+        self.door_choice.set(None)
+
+        a = tk.Radiobutton(self.parent, text='A', variable=self.door_choice,
+                           value='a', command=self.win_reveal)
+        b = tk.Radiobutton(self.parent, text='B', variable=self.door_choice,
+                           value='b', command=self.win_reveal)
+        c = tk.Radiobutton(self.parent, text='C', variable=self.door_choice,
+                           value='c', command=self.win_reveal)
+
+        self.change_door = tk.StringVar()
+        self.change_dor.set(None)
+
+        instr_lbl = tk.Label(self.parent, text='Change doors?')
+        instr_lbl.grid(row=2, column=3, columnspan=1, sticky='E')
+
+        self.yes = tk.Radiobutton(self.parent, state='disabled', text='Y',
+                                  variable=self.change_door, value='y',
+                                  command=self.show_final)
+        self.no = tk.Radiobutton(self.parent, state='disabled', text='N',
+                                  variable=self.change_door, value='n',
+                                  command=self.show_final)
+
+        defaultbg = self.parent.cget('bg')
+        self.unchanged_wins_txt = tk.Text(self.parent, width=20,
+                                          height=1, wrap=tk.WORD,
+                                          bg=defaultbg, fg='black',
+                                          borderwidth=0)
+        self.changed_wins_txt = tk.Text(self.parent, width=20,
+                                        height=1, wrap=tk.WORD,
+                                        bg=defaultbg, fg='black',
+                                        borderwidth=0)
+
+
 
