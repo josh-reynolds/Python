51a8c315e9226b2c3a16d8ea34e2c0be8eeb590e
josh-reynolds <josh.reynolds@frontier.com>
2025-07-11 11:38:45 -0700

    Complete input collection in nest_egg_mcs script

--- IPP/twelve/nest_egg_mcs.py
+++ IPP/twelve/nest_egg_mcs.py
@@ -3,6 +3,10 @@ import sys
 #import random
 #import matplotlib.pyplot as plt
 
+def pr_red(output):
+    """Print string to console, colored red."""
+    print(f"\033[91m{output}\033[00m")
+
 def read_to_list(file_name):
     """Open a file of data in percent, convert to decimal & return a list."""
     with open(file_name, 'r', encoding='utf-8') as in_file:
@@ -48,3 +52,34 @@ start_value = default_input("Input starting value of investments: \n", \
                             '2000000')
 while not start_value.isdigit():
     start_value = input("Invalid input! Input integer only: ")
+
+withdrawal = default_input("Input annual pre-tax withdrawal" \
+                           " (today's $): \n", '80000')
+while not withdrawal.isdigit():
+    withdrawal = input("Invalid input! Input integer only: ")
+
+min_years = default_input("Input minimum years in retirement: \n", '18')
+while not min_years.isdigit():
+    min_years = input("Invalid input! Input integer only: ")
+
+most_likely_years = default_input("Input most-likely years in retirement: \n",
+                                  '25')
+while not most_likely_years.isdigit():
+    most_likely_years = input("Invalid input! Input integer only: ")
+
+max_years = default_input("Input maximum years in retirement: \n",
+                          '40')
+while not max_years.isdigit():
+    max_years = input("Invalid input! Input integer only: ")
+
+num_cases = default_input("Input number of cases to rn: \n", '50000')
+while not num_cases.isdigit():
+    num_cases = input("Invalid input! Input integer only: ")
+
+if (not int(min_years) < int(most_likely_years) < int(max_years)
+    or int(max_years) > 99):
+    pr_red("\nProblem with input years.")
+    pr_red("Requires Min < ML < Max with Max <= 99")
+    sys.exit(1)
+
+
