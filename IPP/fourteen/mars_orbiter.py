74669f506963689938355505bc8e45b5d05caf5e
josh-reynolds <josh.reynolds@frontier.com>
2025-07-13 11:35:17 -0700

    Starting setup for Mars Orbiter project

---
+++ IPP/fourteen/mars_orbiter.py
@@ -0,0 +1,11 @@
+"""Game simulating satellite orbiting Mars."""
+#import os
+#import math
+#import random
+#import pygame as pg
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+LT_BLUE = (173,216,230)
