3804a156a34bb8600e10b99881e2877bd711d8aa
josh-reynolds <josh.reynolds@frontier.com>
2025-07-16 16:04:41 -0700

    Adding crop_images() function and completing crop_n_scale_images script

--- IPP/fifteen/crop_n_scale_images.py
+++ IPP/fifteen/crop_n_scale_images.py
@@ -1,8 +1,8 @@
 """Crop and scale a set of images to facilitate image stacking operations."""
 import os
-#import sys
 import shutil
-#from PIL import Image, ImageOps
+from PIL import Image, ImageOps
+# pylint: disable=C0103, E1101
 
 def main():
     """Get starting folder, copy folder, run crop function & clean folder."""
@@ -33,4 +33,19 @@ def clean_folder(prefix_to_save):
             os.remove(file)
 
 def crop_images():
-    pass
+    """Crop and scale images of a planet to box around planet."""
+    files = os.listdir()
+    for file_num, file in enumerate(files, start=1):
+        with Image.open(file) as img:
+            gray = img.convert('L')
+            bw = gray.point(lambda x: 0 if x < 90 else 255)
+            box = bw.getbbox()
+            padded_box = (box[0]-20, box[1]-20, box[2]+20, box[3]+20)
+            cropped = img.crop(padded_box)
+            scaled = ImageOps.fit(cropped, (860,860),
+                                  Image.LANCZOS, 0, (0.5, 0.5))
+            file_name = f'cropped_{file_num}.jpg'
+            scaled.save(file_name, "JPEG")
+
+if __name__ == '__main__':
+    main()
