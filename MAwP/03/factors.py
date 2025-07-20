b0fc203f620fba5c91c4f00bda7e5738ea72b301
josh-reynolds <josh.reynolds@frontier.com>
2025-07-20 12:02:36 -0700

    Exercise 3-1: Finding greatest common factor of two integers

--- MAwP/03/factors.py
+++ MAwP/03/factors.py
@@ -8,10 +8,26 @@ def factors(num):
             factor_list.append(i)
     return factor_list
 
+def gcf(num1, num2):
+    """Find Greatest Common Factor of two munbers."""
+    fact1 = factors(num1)
+    fact2 = factors(num2)
+    greatest = 0
+    for num in fact1:
+        if num in fact2 and num > greatest:
+            greatest = num
+    return greatest
+
+print("Greatest Common Factor\n")
 while True:
-    number = input("Enter a number (or 'q' to quit): ")
-    if number == 'q':
+    number1 = input("Enter the first number (or 'q' to quit): ")
+    if number1 == 'q':
         break
-    fact = factors(int(number))
-    print(f"The factors of {number} are:")
-    print(fact)
+
+    number2 = input("Enter the second number (or 'q' to quit): ")
+    if number2 == 'q':
+        break
+
+    great = gcf(int(number1), int(number2))
+    print(f"The greatest common factor of {number1} and ",
+          f"{number2} is {great}\n")
