52c874fc0bf7b708c20b1f9478fad1c8eb20e34f
josh-reynolds <josh.reynolds@frontier.com>
2025-07-21 13:54:07 -0700

    Adding plug() function to brute force solution test of a cubic equation

--- MAwP/04/equation.py
+++ MAwP/04/equation.py
@@ -12,6 +12,21 @@ def quad(a,b,c):
     x2 = (-b - sqrt(b**2 - 4*a*c))/(2*a)
     return x1, x2
 
+def g(x):
+    """Cubic equation."""
+    return 6*x**3 + 31*x**2 + 3*x - 10
+
+def plug():
+    """Brute force test for solutions to a cubic equation."""
+    x = -100
+    solution = []
+    while x < 100:
+        if g(x) == 0:
+            solution.append(x)
+        x += 1
+    return solution
+
 print(f"The solution for 2x + 5 = 13 is {equation(2,5,0,13)}")
 print(f"The solution for 12x + 18 = -34x + 67 is {equation(12,18,-34,67)}")
 print(f"The solutions for 2x^2 + 7x -15 = 0 are {quad(2,7,-15)}")
+print(f"A solution for 6x^3 + 31x^2 + 3x - 10 = 0 is {plug()}")
