f3076b50fa6ce261e99fdd4068184d91fc60a797
josh-reynolds <josh.reynolds@frontier.com>
2025-07-21 14:02:38 -0700

    Bringing in engine module from NOC projects, and setting up basic main script for graphing application

---
+++ MAwP/04/main.py
@@ -0,0 +1,14 @@
+"""Display equations on a graph."""
+from engine import run
+
+WIDTH = 400
+HEIGHT = 400
+TITLE = "Graphing Equations"
+
+def update():
+    """Update state once per frame."""
+
+def draw():
+    """Draw on window once per frame."""
+
+run()
