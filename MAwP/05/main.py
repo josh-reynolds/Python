14330c7730e2d772d8c7c126f9aaedc08dbc7162
josh-reynolds <josh.reynolds@frontier.com>
2025-07-23 12:06:20 -0700

    Circle of circles using rotate transform

--- MAwP/05/main.py
+++ MAwP/05/main.py
@@ -1,6 +1,7 @@
 """Chapter 5 - Transforming Shapes with Geometry."""
+from math import radians
 from engine import run
-from screen_matrix import rect, translate, push_matrix, pop_matrix
+from screen_matrix import circle, translate, rotate, push_matrix, pop_matrix
 
 WIDTH = 600
 HEIGHT = 600
@@ -13,7 +14,9 @@ def draw():
     """Draw to window once per frame."""
     push_matrix()
     translate(WIDTH/2, HEIGHT/2)
-    rect(50,100,100,60)
+    for i in range(12):
+        circle(200,0,25)
+        rotate(radians(360/12))
     pop_matrix()
 
 run()
