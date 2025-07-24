1b9cc48ed35840a7df62bd3558931f3d012b1f56
josh-reynolds <josh.reynolds@frontier.com>
2025-07-24 16:47:23 -0700

    Grid of squares, dynamically colored by distance from mouse

--- MAwP/05/main.py
+++ MAwP/05/main.py
@@ -1,5 +1,6 @@
 """Chapter 5 - Transforming Shapes with Geometry."""
-from math import radians
+from math import radians, dist
+import pygame
 from engine import run
 from screen_matrix import push_matrix, translate, rotate, rect, pop_matrix
 # pylint: disable=C0103, W0603
@@ -8,25 +9,37 @@ WIDTH = 600
 HEIGHT = 600
 TITLE = "Geometry"
 
-time = 0
+#time = 0
 
 def update():
     """Update app state once per frame."""
 
 def draw():
     """Draw to window once per frame."""
-    global time
-    push_matrix()
-    translate(WIDTH/2, HEIGHT/2)
-    rotate(radians(time))
-    for _ in range(12):
-        push_matrix()
-        translate(200,0)
-        rotate(radians(time))
-        rect(0,0,50,50)
-        pop_matrix()
-        rotate(radians(360/12))
-    pop_matrix()
-    time += 1
+    #global time
+    #push_matrix()
+    #translate(WIDTH/2, HEIGHT/2)
+    #rotate(radians(time))
+    #for _ in range(12):
+        #push_matrix()
+        #translate(200,0)
+        #rotate(radians(time))
+        #rect(0,0,50,50)
+        #pop_matrix()
+        #rotate(radians(360/12))
+    #pop_matrix()
+    #time += 1
+
+    screen.fill((0,0,0))
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    for x in range(20):
+        for y in range(20):
+            # TO_DO: rect is distorted with new implementation, investigate
+            d = dist((30*x, 30* y), (mouse_x, mouse_y))
+            value = int(0.5*d % 255)
+            # TO_DO: book implementation uses HSV, I just have RGB here...
+            color = (value, value//2, value)
+
+            rect(30*x, 30*y, 25, 25, color)
 
 run()
