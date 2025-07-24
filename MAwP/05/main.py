a0802e2fddd33abd5dbf65d6659d3446a33851ca
josh-reynolds <josh.reynolds@frontier.com>
2025-07-24 09:15:00 -0700

    Animating circle of squares

--- MAwP/05/main.py
+++ MAwP/05/main.py
@@ -7,16 +7,21 @@ WIDTH = 600
 HEIGHT = 600
 TITLE = "Geometry"
 
+time = 0
+
 def update():
     """Update app state once per frame."""
 
 def draw():
     """Draw to window once per frame."""
+    global time
     push_matrix()
     translate(WIDTH/2, HEIGHT/2)
+    rotate(radians(time))
     for i in range(12):
         rect(200,0,50,50)
         rotate(radians(360/12))
     pop_matrix()
+    time += 1
 
 run()
