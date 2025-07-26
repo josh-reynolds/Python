27f58cb09b91232e8d0a2ac8c6ba10f2cdb6ec41
josh-reynolds <josh.reynolds@frontier.com>
2025-07-26 12:37:50 -0700

    Drawing a hexagon using trig functions

--- MAwP/06/main.py
+++ MAwP/06/main.py
@@ -1,4 +1,5 @@
 """Chapter 6 - Creating Oscillations with Trigonometry."""
+from math import radians, cos, sin
 from engine import run
 from screen_matrix import push_matrix, translate, pop_matrix, polygon
 
@@ -14,7 +15,13 @@ def draw():
     """Draw to window once per frame."""
     push_matrix()
     translate(WIDTH/2, HEIGHT/2)
-    polygon(((100,100), (100,200), (200,200), (200,100), (150,50)))
+    # TO_DO: Maybe. Depends if we want to closely emulate Processing.
+    #        The model there is to use beginShape, vertex and endShape.
+    vertices = []
+    for i in range(6):
+        vertices.append((100 * cos(radians(60 * i)),
+                         100 * sin(radians(60 * i))))
+    polygon(vertices)
     pop_matrix()
 
 
