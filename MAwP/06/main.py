52b39b4ecbcc895104a2937e369593b3ea241205
josh-reynolds <josh.reynolds@frontier.com>
2025-07-27 11:50:18 -0700

    Orbiting a circle and showing oscillating motion

--- MAwP/06/main.py
+++ MAwP/06/main.py
@@ -1,11 +1,15 @@
 """Chapter 6 - Creating Oscillations with Trigonometry."""
 from math import radians, cos, sin
 from engine import run
-from screen_matrix import push_matrix, translate, pop_matrix, polygon
+from screen_matrix import push_matrix, translate, pop_matrix, polygon, circle, line
 
 WIDTH = 600
 HEIGHT = 600
 TITLE = "Trigonometry"
+
+R1 = 100 # radius of big circle
+R2 = 10  # radius of small circle
+t = 0
 
 def poly(sides, sz):
     """Draw a polygon of arbitrary size and number of sides."""
@@ -23,10 +27,28 @@ def update():
 
 def draw():
     """Draw to window once per frame."""
+    global t
+
+    #push_matrix()
+    #translate(WIDTH/2, HEIGHT/2)
+    #poly(3,100)
+    #pop_matrix()
+
     push_matrix()
-    translate(WIDTH/2, HEIGHT/2)
-    poly(3,100)
+
+    translate(WIDTH/4, HEIGHT/2)
+    circle(0,0,R1)
+
+    x = R1 * cos(t)
+    y = R1 * sin(t)
+
+    line(x,y,200,y)
+    circle(200,y,R2,(0,255,0),0)
+    circle(x,y,R2,(255,0,0),0)
+
     pop_matrix()
+
+    t += 0.05
 
 
 run()
