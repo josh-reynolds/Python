c8616c2b0ea1793d352198eadf907f5ffaef6630
josh-reynolds <josh.reynolds@frontier.com>
2025-07-28 16:09:40 -0700

    Starting setup for Complex Numbers chapter

---
+++ MAwP/07/main.py
@@ -0,0 +1,14 @@
+"""Chapter 7 - Complex Numbers."""
+from engine import run
+
+WIDTH = 600
+HEIGHT = 600
+TITLE = "Complex Numbers"
+
+def update():
+    """Update app state once per frame."""
+
+def draw():
+    """Draw to window once per frame."""
+
+run()
