9175120e435a3e7073119125a534385ed37e56c6
josh-reynolds <josh.reynolds@frontier.com>
2025-08-02 10:40:45 -0700

    Simple bouncing ball script

--- MAwP/09/main.py
+++ MAwP/09/main.py
@@ -1,5 +1,6 @@
 """Chapter 9 - Building Objects with Classes."""
 from engine import run
+from screen_matrix import circle
 
 WIDTH = 600
 HEIGHT = 600
@@ -10,5 +11,21 @@ def update():
 
 def draw():
     """Draw to the window once per frame."""
+    global xcor, ycor, xvel, yvel
+    screen.fill((0,0,0))
+    xcor += xvel
+    ycor += yvel
+
+    if xcor > WIDTH or xcor < 0:
+        xvel = -xvel
+    if ycor > HEIGHT or ycor < 0:
+        yvel = -yvel
+
+    circle(xcor, ycor, 10, (255,255,255), 0)
+
+xcor = 300
+ycor = 300
+xvel = 1
+yvel = 2
 
 run()
