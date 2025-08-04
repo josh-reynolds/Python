c7c492b2eac7046e1af85245e971c424f503720d
josh-reynolds <josh.reynolds@frontier.com>
2025-08-04 15:49:20 -0700

    Dynamically changing tree branch angle via mouse input

--- MAwP/10/main.py
+++ MAwP/10/main.py
@@ -10,16 +10,19 @@ TITLE = "Fractals & Recursion"
 
 def tree_fork(size, level):
     """Draw a fork consisting of a trunk and two branches."""
+    _, mouse_y = pygame.mouse.get_pos()
+    angle = remap(mouse_y, 0, HEIGHT, 0, 180)
+
     if level > 0:
         line(0,0,0,-size)
         translate(0, -size)
-        rotate(radians(30))
+        rotate(radians(angle))
         
         tree_fork(0.8*size, level-1)
-        rotate(radians(-60))
+        rotate(radians(-2*angle))
 
         tree_fork(0.8*size, level-1)
-        rotate(radians(30))
+        rotate(radians(angle))
 
         translate(0, size)
 
@@ -29,7 +32,7 @@ def update():
 def draw():
     """Draw to the window once per frame."""
     mouse_x, _ = pygame.mouse.get_pos()
-    level = int(remap(mouse_x, 0, WIDTH, 0, 10))
+    level = int(remap(mouse_x, 0, WIDTH, 0, 15))
     push_matrix()
     translate(300,500)
     tree_fork(100,level)
