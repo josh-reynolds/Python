6987a225225b32659f55bedce8702e3e5607aeac
josh-reynolds <josh.reynolds@frontier.com>
2025-08-07 14:19:11 -0700

    Starting setup for Chapter 11 - Cellular Automata

---
+++ MAwP/11/main.py
@@ -0,0 +1,14 @@
+"""Chapter 11 - Cellular Automata."""
+from engine import run
+
+WIDTH = 600
+HEIGHT = 600
+TITLE = "Cellular Automata"
+
+def update():
+    """Update the app state once per frame."""
+
+def draw():
+    """Draw to the window once per frame."""
+
+run()
