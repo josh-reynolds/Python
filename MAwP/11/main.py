796c9790af653c72476eae47126bcb99334d07d4
josh-reynolds <josh.reynolds@frontier.com>
2025-08-07 14:23:29 -0700

    Drawing a simple grid

--- MAwP/11/main.py
+++ MAwP/11/main.py
@@ -1,14 +1,23 @@
 """Chapter 11 - Cellular Automata."""
-from engine import run
+from engine import run, screen
 
 WIDTH = 600
 HEIGHT = 600
 TITLE = "Cellular Automata"
+
+GRID_W = 15
+GRID_H = 15
+CELL_SIZE = 18
 
 def update():
     """Update the app state once per frame."""
 
 def draw():
     """Draw to the window once per frame."""
+    for x in range(GRID_W):
+        for y in range(GRID_H):
+            screen.draw.rect(CELL_SIZE*x, CELL_SIZE* y, 
+                             CELL_SIZE, CELL_SIZE, 
+                             color=(0,0,0), width=1)
 
 run()
