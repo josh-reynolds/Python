be694fd7f0de2d9d242e17ef0673779bd6fcf6be
josh-reynolds <josh.reynolds@frontier.com>
2025-08-08 12:44:30 -0700

    Generating more rows of Elementary CA

--- MAwP/11/main.py
+++ MAwP/11/main.py
@@ -7,9 +7,24 @@ WIDTH = 600
 HEIGHT = 600
 TITLE = "Cellular Automata"
 
-w = 50
-rows = 1
-cols = 11
+w = 3
+rows = 1000
+cols = 1000
+
+def rules(a,b,c):
+    """Evaluate CA ruleset."""
+    return ruleset[7 - (4*a + 2*b + c)]
+
+def generate():
+    """Create next row in CA."""
+    for i, row in enumerate(cells):
+        for j in range(1, len(row)-1):
+            left = row[j-1]
+            me = row[j]
+            right = row[j+1]
+            if i < len(cells) - 1:
+                cells[i+1][j] = rules(left, me, right)
+    return cells
 
 def update():
     """Update the app state once per frame."""
@@ -20,13 +35,14 @@ def draw():
         for j, v in enumerate(cell):
             color = (0,0,0) if v == 1 else (255,255,255)
             rect(j*w-(cols*w-WIDTH)/2, w*i, w, w, color, 0)
-            rect(j*w-(cols*w-WIDTH)/2, w*i, w, w, (0,0,0), 1)
 
+ruleset = [0,0,0,1,1,1,1,0]
 cells = []
 for r in range(rows):
     cells.append([])
     for c in range(cols):
         cells[r].append(0)
 cells[0][cols//2] = 1
+cells = generate()
 
 run()
