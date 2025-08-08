9010f5bf476901023e810d98287b01e0d2843383
josh-reynolds <josh.reynolds@frontier.com>
2025-08-08 12:34:03 -0700

    Basic setup for Elementary CA

--- MAwP/11/main.py
+++ MAwP/11/main.py
@@ -1,103 +1,32 @@
 """Chapter 11 - Cellular Automata."""
-from random import choice
-from engine import run, screen
-# pylint: disable=C0103, E1121, W0603, E1101
+from engine import run
+from screen_matrix import rect
+# pylint: disable=C0103
 
 WIDTH = 600
 HEIGHT = 600
 TITLE = "Cellular Automata"
 
-GRID_W = 50
-GRID_H = 50
-CELL_SIZE = WIDTH // GRID_W
-
-generation = 1
-key_down = False
-frame_counter = 0
-
-class Cell:
-    """Cell class for Cellular Automata."""
-
-    def __init__(self, column, row, state=0):
-        """Create a Cell object."""
-        self.column = column
-        self.row = row
-        self.state = state
-
-    def display(self):
-        """Draw Cell on screen."""
-        if self.state == 1:
-            color = (0,0,0)
-        else:
-            color = (255,255,255)
-
-        screen.draw.rect(CELL_SIZE*self.row, CELL_SIZE* self.column,
-                         CELL_SIZE, CELL_SIZE,
-                         color, 0)
-        screen.draw.rect(CELL_SIZE*self.row, CELL_SIZE* self.column,
-                         CELL_SIZE, CELL_SIZE,
-                         (220,220,220), 1)
-
-    def check_neighbors(self):
-        """Examine states of neighbor cells."""
-        neighbors = 0
-
-        for dr,dc in [[-1,-1],[-1,0],[-1,1],
-                      [0,-1],[0,1],
-                      [1,-1],[1,0],[1,1]]:
-            try:
-                if cell_list[self.row + dr][self.column + dc].state == 1:
-                    neighbors += 1
-            except IndexError:
-                continue
-
-        if self.state == 1:
-            if neighbors in [2,3]:
-                return 1
-            return 0
-        if neighbors == 3:
-              return 1
-        return 0
-
-def create_cell_list():
-    """Create a list of Cells with one on Cell in the center."""
-    new_list = []
-    for j in range(GRID_H):
-        new_list.append([])
-        for i in range(GRID_W):
-            new_list[j].append(Cell(i,j,choice([0,1])))
-
-    new_list[GRID_H//2][GRID_W//2].state = 1
-    return new_list
-
-def update_cell_list(c_list):
-    """Create next generation of a CA using a double-buffer."""
-    new_list = []
-    for r,row in enumerate(c_list):
-        new_list.append([])
-        for c,cell in enumerate(row):
-            new_list[r].append(Cell(c,r,cell.check_neighbors()))
-    return new_list[::]
+w = 50
+rows = 1
+cols = 11
 
 def update():
     """Update the app state once per frame."""
 
 def draw():
     """Draw to the window once per frame."""
-    global generation, cell_list, frame_counter
-    if frame_counter % 20 == 0:
-        cell_list = update_cell_list(cell_list)
-        generation += 1
+    for i, cell in enumerate(cells):
+        for j, v in enumerate(cell):
+            color = (0,0,0) if v == 1 else (255,255,255)
+            rect(j*w-(cols*w-WIDTH)/2, w*i, w, w, color, 0)
+            rect(j*w-(cols*w-WIDTH)/2, w*i, w, w, (0,0,0), 1)
 
-    frame_counter += 1
-
-    for row in cell_list:
-        for cell in row:
-            cell.display()
-
-    screen.draw.text(f"Generation: {generation}", pos=(460,20),
-                     color=(255,0,0))
-
-cell_list = create_cell_list()
+cells = []
+for r in range(rows):
+    cells.append([])
+    for c in range(cols):
+        cells[r].append(0)
+cells[0][cols//2] = 1
 
 run()
