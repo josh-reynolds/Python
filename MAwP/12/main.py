c904577a635b2a5e85e2e97850cbfa9dd51d1059
josh-reynolds <josh.reynolds@frontier.com>
2025-08-09 13:38:17 -0700

    Adding Route class and displaying it

--- MAwP/12/main.py
+++ MAwP/12/main.py
@@ -1,12 +1,14 @@
 """Chapter 12 - Genetic Algorithms."""
-from random import randint
-from engine import run
-from screen_matrix import circle
+from random import randint, sample
+from engine import run, screen
+from screen_matrix import circle, polygon
 # pylint: disable=C0103, R0903
 
 WIDTH = 600
 HEIGHT = 600
 TITLE = "Genetic Algorithms"
+
+CITY_COUNT = 10
 
 class City:
     """An object representing a city location."""
@@ -21,9 +23,28 @@ class City:
         """Draw the City on the window surface."""
         color = (0,255,255)
         circle(self.x, self.y, 10, color, 0)
-        screen.draw.text(str(self.number), 
+        screen.draw.text(str(self.number),
                          center=(self.x, self.y-20),
                          color=color)
+
+class Route:
+    """A series of links between cities."""
+
+    def __init__(self):
+        """Create a Route object."""
+        self.distance = 0
+        self.city_nums = sample(list(range(CITY_COUNT)),
+                                CITY_COUNT)
+
+    def display(self):
+        """Draw the Route on the window surface."""
+        color = (255,0,255)
+        points = []
+        for j in self.city_nums:
+            points.append((cities[j].x, cities[j].y))
+        polygon(points, color, 1)
+        for j in self.city_nums:
+            cities[j].display()
 
 def update():
     """Update the app state once per frame."""
@@ -31,14 +52,14 @@ def update():
 def draw():
     """Draw to the window once per frame."""
     screen.fill((0,0,0))
-    for city in cities:
-        city.display()
+    r.display()
 
 cities = []
-for i in range(6):
+for i in range(CITY_COUNT):
     cities.append(City(randint(50, WIDTH-50),
                        randint(50, HEIGHT-50),
                        i))
 
+r = Route()
 
 run()
