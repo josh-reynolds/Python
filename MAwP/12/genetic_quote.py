66078248c1c9529ef3a478185469104f9ce85407
josh-reynolds <josh.reynolds@frontier.com>
2025-08-09 13:17:48 -0700

    Applying pylint/pydocstyle feedback to string-guessing script

--- MAwP/12/genetic_quote.py
+++ MAwP/12/genetic_quote.py
@@ -1,21 +1,24 @@
 """Guess a quote using a genetic algorithm."""
 import random
+# pylint: disable=C0103
 
-target = "I never go back on my word, because that is my Ninja way."
-characters = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.',?!"
+TARGET = "I never go back on my word, because that is my Ninja way."
+CHARACTERS = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.',?!"
 
 def make_list():
-    """Returns a list of characters the same length as the target."""
+    """Return a list of CHARACTERS the same length as the TARGET."""
     char_list = []
-    for i in range(len(target)):
-        char_list.append(random.choice(characters))
+    for _ in range(len(TARGET)):
+        char_list.append(random.choice(CHARACTERS))
     return char_list
 
 def score(a_list):
-    """Return number of matches between a_list and the target."""
+    """Return number of matches between a_list and the TARGET."""
     matches = 0
-    for i in range(len(a_list)):
-        if a_list[i] == target[i]:
+    for i, letter in enumerate(a_list):
+    #for i in range(len(a_list)):
+        if letter == TARGET[i]:
+        #if a_list[i] == TARGET[i]:
             matches += 1
     return matches
 
@@ -24,10 +27,10 @@ def to_string(a_list):
     return ''.join(a_list)
 
 def mutate(a_list):
-    """Returns a_list with one letter changed."""
+    """Return a_list with one letter changed."""
     new_list = list(a_list)
-    new_letter = random.choice(characters)
-    index = random.randint(0, len(target)-1)
+    new_letter = random.choice(CHARACTERS)
+    index = random.randint(0, len(TARGET)-1)
     new_list[index] = new_letter
     return new_list
 
@@ -42,10 +45,11 @@ while True:
     guesses += 1
     if guess_score <= best_score:
         continue
-    else:
-        print(to_string(guess), guess_score, guesses)
-        if guess_score == len(target):
-            break
-        else:
-            best_list = list(guess)
-            best_score = guess_score
+
+    print(to_string(guess), guess_score, guesses)
+
+    if guess_score == len(TARGET):
+        break
+
+    best_list = list(guess)
+    best_score = guess_score
