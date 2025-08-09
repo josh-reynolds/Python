dc813116537eddbb271df873e6e293c48d3e5d3b
josh-reynolds <josh.reynolds@frontier.com>
2025-08-09 13:09:31 -0700

    Starting setup for Traveling Salesperson Problem

---
+++ MAwP/12/main.py
@@ -0,0 +1,14 @@
+"""Chapter 12 - Genetic Algorithms."""
+from engine import run
+
+WIDTH = 600
+HEIGHT = 600
+TITLE = "Genetic Algorithms"
+
+def update():
+    """Update the app state once per frame."""
+
+def draw():
+    """Draw to the window once per frame."""
+
+run()
