c996821a2e1cad8af2fdeca94ef19360fb58e184
josh-reynolds <josh.reynolds@frontier.com>
2025-08-24 17:10:21 -0700

    Rudimentary framework for a Traveller trading game

---
+++ RPGs/traveller/traveller.py
@@ -0,0 +1,27 @@
+def pr_red(string):
+    """Print string to console, colored red."""
+    print(f"\033[91m {string}\033[00m")
+
+world = "Yorbund"
+hold = []
+
+while True:
+    pr_red(f"\nYou are on {world}.")
+    command = input("Enter a command (? to list).  ")
+    if command.lower() == 'q':
+        break
+    if command.lower() == '?':
+        # TO_DO: commands should be location-dependent
+        print("? - List commands")
+        print("c - Cargo hold contents")
+        print("j - Jump to new system")
+        print("l - Lift off to orbit")
+        print("q - Quit")
+        print("t - Trade")
+    if command.lower() == 'c':
+        print("Contents of cargo hold:")
+        for item in hold:
+            print(item)
+
+print("Goodbye.")
+
