29d9b7baaf5d188042bc53f902b161b707200715
josh-reynolds <josh.reynolds@frontier.com>
2025-09-21 22:15:11 -0700

    Adding initial test and implementation for StarMap.get_system_at_coordinate()

--- RPGs/traveller/star_map.py
+++ RPGs/traveller/star_map.py
@@ -5,6 +5,12 @@ class StarMap:
     def get_systems_within_range(self, origin, distance):
         return [StarSystem("Kinorb", "A", 5, 5, 5, 5)]
 
+    def get_system_at_coordinate(self, coordinate):
+        if coordinate == (0,0,0):
+            return StarSystem("Yorbund", "A", 5, 5, 5, 5)
+        return StarSystem("Kinorb", "A", 5, 5, 5, 5)
+
+
 class StarMapTestCase(unittest.TestCase):
     def setUp(self):
         StarMapTestCase.star_map = StarMap()
@@ -15,9 +21,22 @@ class StarMapTestCase(unittest.TestCase):
 
     def test_get_systems_within_range(self):
         star_map = StarMapTestCase.star_map
+
         systems = star_map.get_systems_within_range((0,0,0), 1)
         self.assertEqual(len(systems), 1)
         self.assertTrue(isinstance(systems[0], StarSystem))
+        self.assertEqual(systems[0].name, "Kinorb")
+
+    def test_get_system_at_coordinate(self):
+        star_map = StarMapTestCase.star_map
+
+        world = star_map.get_system_at_coordinate((0,0,0))
+        self.assertTrue(isinstance(world, StarSystem))
+        self.assertEqual(world.name, "Yorbund")
+
+        world = star_map.get_system_at_coordinate((1,0,-1))
+        self.assertTrue(isinstance(world, StarSystem))
+        self.assertEqual(world.name, "Kinorb")
 
 # -------------------------------------------------------------------
 if __name__ == '__main__':
