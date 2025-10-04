bb095521ba569eaa430492a627c4f73667845943
josh-reynolds <josh.reynolds@frontier.com>
2025-10-03 17:24:54 -0700

    Moving ship tests to a separate module

--- RPGs/traveller/unit_tests.py
+++ RPGs/traveller/unit_tests.py
@@ -7,7 +7,7 @@ def suite():
     test_suite.addTests(loader.loadTestsFromName('test_calendar'))
     test_suite.addTests(loader.loadTestsFromName('test_cargo'))
     test_suite.addTests(loader.loadTestsFromName('test_financials'))
-    test_suite.addTests(loader.loadTestsFromName('ship'))
+    test_suite.addTests(loader.loadTestsFromName('test_ship'))
     test_suite.addTests(loader.loadTestsFromName('star_map'))
     test_suite.addTests(loader.loadTestsFromName('star_system'))
     test_suite.addTests(loader.loadTestsFromName('utilities'))
