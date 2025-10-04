288bf1d333740f3a05484f2762748851b39d9ce2
josh-reynolds <josh.reynolds@frontier.com>
2025-10-03 19:17:14 -0700

    Moving star_map tests to a separate module

--- RPGs/traveller/unit_tests.py
+++ RPGs/traveller/unit_tests.py
@@ -8,7 +8,7 @@ def suite():
     test_suite.addTests(loader.loadTestsFromName('test_cargo'))
     test_suite.addTests(loader.loadTestsFromName('test_financials'))
     test_suite.addTests(loader.loadTestsFromName('test_ship'))
-    test_suite.addTests(loader.loadTestsFromName('star_map'))
+    test_suite.addTests(loader.loadTestsFromName('test_star_map'))
     test_suite.addTests(loader.loadTestsFromName('star_system'))
     test_suite.addTests(loader.loadTestsFromName('utilities'))
     test_suite.addTests(loader.loadTestsFromName('word_gen'))
