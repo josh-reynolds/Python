d8138a72916a54eef773758cbc50aa5cf4f086e4
josh-reynolds <josh.reynolds@frontier.com>
2025-11-23 23:18:25 -0800

    Starting setup for Voronoi project

---
+++ pygame/voronoi/main.py
@@ -0,0 +1,14 @@
+"""Fill a canvas with Voronoi cells."""
+from engine import screen, run
+
+WIDTH = 600
+HEIGHT = 690
+TITLE = "Voronoi Cells"
+
+def update():
+    pass
+
+def draw():
+    screen.fill((200,200,200))
+
+run()
