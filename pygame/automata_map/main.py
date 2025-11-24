990ad675b46a25bab615687989abb7fda5a2b73f
josh-reynolds <josh.reynolds@frontier.com>
2025-11-24 12:39:21 -0800

    Finishing cellular automata - seems too aggressive, need to assess

--- pygame/automata_map/main.py
+++ pygame/automata_map/main.py
@@ -1,4 +1,18 @@
-"""Generate a map image using cellular automata."""
+"""Generate a map image using cellular automata.
+
+Derived from "Cellular Automata Procedural Development"
+by White Box Dev (YouTube).
+"""
+# Moore neighborhood
+# if > 4 are wall -> wall
+# if <= 4 are wall -> floor
+# out of bounds coordinates are walls
+
+# generate noise
+# run an iteration
+# repeat
+
+from random import randint
 from engine import screen, run
 
 WIDTH = 800
@@ -9,14 +23,52 @@ CELL_SIZE = 200
 COLUMNS = WIDTH // CELL_SIZE
 ROWS = HEIGHT // CELL_SIZE
 
+grid = [[randint(0,1) for x in range(COLUMNS)] for y in range(ROWS)]
+
 def update():
-    pass
+    global counter
+    if counter % 300 == 0:
+        generate()
+    counter += 1
 
 def draw():
     screen.fill((200,200,200))
 
-    for x in range(COLUMNS):
-        for y in range(ROWS):
-            screen.draw.rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, (255,0,200), 1)
+    for y in range(ROWS):
+        for x in range(COLUMNS):
+            color = (255,0,200)
+            if grid[y][x] == 0:
+                color = (0,0,0)
+
+            screen.draw.rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, color, 0)
+
+def generate():
+    print("generating iteration")
+    temp_grid = grid[:]
+    for j in range(ROWS):
+        for k in range(COLUMNS):
+            neighbor_wall_count = count_neighbors(j,k,temp_grid)
+            print(f"{j},{k} state = {grid[j][k]} neighbors = {neighbor_wall_count}")
+            if neighbor_wall_count > 4:
+                grid[j][k] = 0
+            else:
+                grid[j][k] = 1
+
+def count_neighbors(row, column, temp_grid):
+    count = 0
+    for y in range(row-1, row+2):
+        for x in range(column-1, column+2):
+            if in_bounds(x,y):
+                if not(y == row) or not(x == column):
+                    if temp_grid[y][x] == 0:
+                        count += 1
+            else:
+                count += 1
+    return count
+
+def in_bounds(x,y):
+    return x >= 0 and y >= 0 and x < COLUMNS and y < ROWS
+
+counter = 1
 
 run()
