e1a9ff57c8ba083d630ea15150d6e3f7f7deb95d
josh-reynolds <josh.reynolds@frontier.com>
2025-11-28 12:31:09 -0800

    Moving Orbit screen class to dedicated module

--- RPGs/traveller/src/menu.py
+++ RPGs/traveller/src/menu.py
@@ -13,6 +13,7 @@ from src.coordinate import Coordinate, coordinate_from, create_3_axis
 from src.credits import Credits
 from src.financials import financials_from
 from src.freight import Freight
+from src.orbit import Orbit
 from src.passengers import PassageClass, passenger_from
 from src.play import Play
 from src.terminal import Passengers
@@ -416,101 +417,6 @@ class Menu(Screen):
         _ = input("\nPress ENTER key to continue.")
         return self._load_screen("Starport")                  # type: ignore[attr-defined]
 
-
-class Orbit(Play):
-    """Contains commands for the Orbit state."""
-
-    def __init__(self, parent: Any) -> None:
-        """Create an Orbit object."""
-        super().__init__(parent)
-        self.commands += [
-                Command('land', 'Land at starport', self.land),
-                Command('outbound', 'Go to jump point', self.outbound_to_jump),
-                ]
-        self.commands = sorted(self.commands, key=lambda command: command.key)
-
-    def __str__(self) -> str:
-        """Return the string representation of the current screen."""
-        return "Orbit"
-
-    # VIEW COMMANDS ========================================================
-    # STATE TRANSITIONS ====================================================
-    def land(self: ScreenT) -> None | ScreenT:
-        """Move from orbit to the starport."""
-        print(f"{BOLD_BLUE}Landing on {self.parent.location.name}.{END_FORMAT}")
-        if not self.parent.ship.model.streamlined:
-            print("Your ship is not streamlined and cannot land.")
-            return None
-
-        if self.parent.ship.repair_status == RepairStatus.BROKEN:
-            print(f"{BOLD_RED}Drive failure. Cannot land.{END_FORMAT}")
-            return None
-
-        if self.parent.ship.destination == self.parent.location:
-            if self.parent.ship.total_passenger_count > 0:
-                print(f"Passengers disembarking on {self.parent.location.name}.")
-
-                funds = Credits(sum(p.ticket_price.amount for p in self.parent.ship.passengers))
-                low_lottery_amount = Credits(10) * self.parent.ship.low_passenger_count
-                funds -= low_lottery_amount
-                print(f"Receiving {funds} in passenger fares.")
-                self.parent.financials.credit(funds, "passenger fare")
-
-                # annotations want ScreenT to have this method
-                self._low_lottery(low_lottery_amount)      # type: ignore[attr-defined]
-
-                self.parent.ship.passengers = []
-                self.parent.ship.hold = [item for item in self.parent.ship.hold
-                                  if not isinstance(item, Baggage)]
-
-        self.parent.location.detail = "starport"
-        self.parent.financials.berthing_fee(self.parent.location.on_surface())
-        return cast(ScreenT, Starport(self.parent))
-
-    def _low_lottery(self, low_lottery_amount) -> None:
-        """Run the low passage lottery and apply results."""
-        if self.parent.ship.low_passenger_count > 0:
-            low_passengers = [p for p in self.parent.ship.passengers if
-                                         p.passage == PassageClass.LOW]
-            for passenger in low_passengers:
-                if die_roll(2) + passenger.endurance + self.parent.ship.medic_skill() < 5:
-                    passenger.survived = False
-
-            survivors = [p for p in low_passengers if p.survived]
-            print(f"{len(survivors)} of {len(low_passengers)} low passengers "
-                  "survived revival.")
-
-            winner = False
-            for passenger in low_passengers:
-                if passenger.guess == len(survivors) and passenger.survived:
-                    winner = True
-
-            if not winner:
-                print(f"No surviving low lottery winner. "
-                      f"The captain is awarded {low_lottery_amount}.")
-                self.parent.financials.credit(low_lottery_amount, "low lottery")
-
-    def outbound_to_jump(self: ScreenT) -> None | ScreenT:
-        """Move from orbit to the jump point."""
-        print(f"{BOLD_BLUE}Travelling out to {self.parent.location.name} jump point.{END_FORMAT}")
-
-        if self.parent.ship.repair_status == RepairStatus.BROKEN:
-            print(f"{BOLD_RED}Drive failure. Cannot travel to the jump point.{END_FORMAT}")
-            return None
-
-        leg_fc = self.parent.ship.model.trip_fuel_cost // 2
-        if self.parent.ship.current_fuel < leg_fc:
-            print(f"Insufficient fuel. Travel out to the jump point "
-                  f"requires {leg_fc} tons, only "
-                  f"{self.parent.ship.current_fuel} tons in tanks.")
-            return None
-
-        self.parent.ship.current_fuel -= leg_fc
-        self.parent.date.day += 1
-        self.parent.location.detail = "jump"
-        return cast(ScreenT, Jump(self.parent))
-
-    # ACTIONS ==============================================================
 
 class Starport(Play):
     """Contains commands for the Starport state."""
