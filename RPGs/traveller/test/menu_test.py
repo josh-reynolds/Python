51b87d6f9eca810e66811b4c2d78b6916d0988a1
josh-reynolds <josh.reynolds@frontier.com>
2025-11-28 11:56:50 -0800

    Moving Passengers screen class to a dedicated module

--- RPGs/traveller/test/menu_test.py
+++ RPGs/traveller/test/menu_test.py
@@ -2,7 +2,7 @@
 import unittest
 from typing import cast, List
 from main import Game
-from src.menu import Menu, Passengers, Trade
+from src.menu import Menu, Trade
 from src.ship import Ship
 from src.star_system import StarSystem
 
@@ -17,52 +17,6 @@ class MenuTestCase(unittest.TestCase):
     def test_update(self):
         """Tests the basic front end menu screen."""
         Menu.update(MenuTestCase.ShipMock("Type A Free Trader"))
-
-class PassengersTestCase(unittest.TestCase):
-    """Test Passengers screen."""
-
-    # pylint: disable=W0212
-    # W0212: Access to a protected member _get_passenger_destinations of a client class
-    @unittest.skip("test has side effects: printing")
-    def test_get_passenger_destinations(self) -> None:
-        """Test getting list of passenger destinations."""
-        game = Game()
-        passengers = Passengers(game)
-        potential_destinations = cast(List[StarSystem], game.location.destinations.copy())
-
-        destinations = passengers._get_passenger_destinations(potential_destinations, 1)
-        print(destinations)
-
-        # contracted destination:
-        #     freight on board
-        #     under contract and in range
-        #     under contract but not in range
-        # else
-
-    # pylint: disable=W0212
-    # W0212: Access to a protected member _select_passengers of a client class
-    @unittest.skip("test has side effects: printing & input")
-    def test_select_passengers(self) -> None:
-        """Test selection of passengers from a list."""
-        game = Game()
-        passengers = Passengers(game)
-        available = (5,5,5)
-        destination = game.location.destinations[0]
-
-        selection = passengers._select_passengers(available, destination)
-        print(selection)
-
-        # no passengers remaining, auto-exit
-        # quit selection
-        # select high passengers
-        # select all high passengers
-        # no cargo space left
-        # select middle passengers
-        # select all middle passengers
-        # no staterooms left
-        # select low passengers
-        # select all low passengers
-        # no low berths left
 
 
 class TradeTestCase(unittest.TestCase):
