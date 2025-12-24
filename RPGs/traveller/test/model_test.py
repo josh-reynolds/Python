00d46b945db5b43d79913d160c4e0a7c8a930de9
josh-reynolds <josh.reynolds@frontier.com>
2025-12-24 14:34:48 -0800

    Adding test for Model.attach_date_observers()

--- RPGs/traveller/test/model_test.py
+++ RPGs/traveller/test/model_test.py
@@ -1,8 +1,14 @@
 """Contains tests for the model module."""
 import unittest
 from src.calendar import Calendar
+from src.cargo_depot import CargoDepot
+from src.coordinate import Coordinate
+from src.financials import Financials
 from src.imperial_date import ImperialDate
 from src.model import Model
+from src.ship import Ship
+from src.star_system import StarSystem
+from src.uwp import UWP
 
 class ModelTestCase(unittest.TestCase):
     """Tests Model class."""
@@ -72,3 +78,18 @@ class ModelTestCase(unittest.TestCase):
             model.load_calendar(string)
         self.assertEqual(f"{context.exception}",
                          "invalid literal for int() with base 10: 'm'")
+
+    def test_attach_date_observers(self) -> None:
+        """Tests attaching observers to the Model calendar."""
+        model = Model()
+        model.date = Calendar()
+        model.map_hex = StarSystem("TEST", Coordinate(1,1,1), UWP('A',1,1,1,1,1,1,1))
+        model.depot = CargoDepot(model.get_star_system(), model.get_current_date())
+        model.financials = Financials(1, model.get_current_date(),
+                                      Ship("Type A Free Trader"), model.get_star_system())
+        self.assertEqual(model.date_string, "001-1105")
+
+        model.attach_date_observers()
+        self.assertEqual(len(model.date.observers), 2)
+        self.assertTrue(isinstance(model.date.observers[0], CargoDepot))
+        self.assertTrue(isinstance(model.date.observers[1], Financials))
