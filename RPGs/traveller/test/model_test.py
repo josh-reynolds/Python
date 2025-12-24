bde77c8e90fcd8e831997d5b00c0c08e8dc2aa92
josh-reynolds <josh.reynolds@frontier.com>
2025-12-24 14:16:22 -0800

    Adding test for Model.load_calendar()

--- RPGs/traveller/test/model_test.py
+++ RPGs/traveller/test/model_test.py
@@ -7,14 +7,6 @@ from src.model import Model
 class ModelTestCase(unittest.TestCase):
     """Tests Model class."""
 
-    def test_add_day(self) -> None:
-        """Tests adding a day to the Model."""
-        model = Model()
-        model.date = Calendar()
-        self.assertEqual(model.date.day, 1)
-        model.add_day()
-        self.assertEqual(model.date.day, 2)
-
     def test_get_current_date(self) -> None:
         """Tests querying current date from the Model."""
         model = Model()
@@ -31,6 +23,14 @@ class ModelTestCase(unittest.TestCase):
         model.add_day()
         self.assertEqual(model.date_string, "002-1105")
 
+    def test_add_day(self) -> None:
+        """Tests adding a day to the Model."""
+        model = Model()
+        model.date = Calendar()
+        self.assertEqual(model.date.day, 1)
+        model.add_day()
+        self.assertEqual(model.date.day, 2)
+
     def test_plus_week(self) -> None:
         """Tests advancing the curret date by a week."""
         model = Model()
@@ -38,3 +38,37 @@ class ModelTestCase(unittest.TestCase):
         self.assertEqual(model.date_string, "001-1105")
         model.plus_week()
         self.assertEqual(model.date_string, "008-1105")
+
+    def test_load_calendar(self) -> None:
+        """Tests applying json data to the Model calendar."""
+        model = Model()
+        model.date = Calendar()
+        self.assertEqual(model.date_string, "001-1105")
+
+        string = "111-1111"
+        model.load_calendar(string)
+        self.assertEqual(model.date_string, string)
+
+        string = "1111"
+        with self.assertRaises(ValueError) as context:
+            model.load_calendar(string)
+        self.assertEqual(f"{context.exception}",
+                         "string must have both day and year values: '1111'")
+
+        string = "1-111-1111"
+        with self.assertRaises(ValueError) as context:
+            model.load_calendar(string)
+        self.assertEqual(f"{context.exception}",
+                         "string must have only day and year values: '1-111-1111'")
+
+        string = "m-1111"
+        with self.assertRaises(ValueError) as context:
+            model.load_calendar(string)
+        self.assertEqual(f"{context.exception}",
+                         "invalid literal for int() with base 10: 'm'")
+
+        string = "11-m"
+        with self.assertRaises(ValueError) as context:
+            model.load_calendar(string)
+        self.assertEqual(f"{context.exception}",
+                         "invalid literal for int() with base 10: 'm'")
