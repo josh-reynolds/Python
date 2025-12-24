a8e0e673ae05932ae57a153a8918312c06712cf3
josh-reynolds <josh.reynolds@frontier.com>
2025-12-23 16:04:34 -0800

    Adding test for Model.get_current_date()

--- RPGs/traveller/test/model_test.py
+++ RPGs/traveller/test/model_test.py
@@ -1,6 +1,7 @@
 """Contains tests for the model module."""
 import unittest
 from src.calendar import Calendar
+from src.imperial_date import ImperialDate
 from src.model import Model
 
 class ModelTestCase(unittest.TestCase):
@@ -13,3 +14,11 @@ class ModelTestCase(unittest.TestCase):
         self.assertEqual(model.date.day, 1)
         model.add_day()
         self.assertEqual(model.date.day, 2)
+
+    def test_get_current_date(self) -> None:
+        """Tests querying current date from the Model."""
+        model = Model()
+        model.date = Calendar()
+        self.assertEqual(model.get_current_date(), ImperialDate(1,1105))
+        model.add_day()
+        self.assertEqual(model.get_current_date(), ImperialDate(2,1105))
