7eeaca973c95f3cdbd80d97e8702157bcb601f83
josh-reynolds <josh.reynolds@frontier.com>
2025-12-26 14:32:31 -0800

    Adding skeletons for WildernessScreen methods

--- RPGs/traveller/src/wilderness.py
+++ RPGs/traveller/src/wilderness.py
@@ -3,7 +3,9 @@
 WildernessScreen - contains commands for the wilderness state.
 """
 from typing import Any
-from src.model import Model
+from src.command import Command
+from src.format import BOLD_BLUE, END_FORMAT
+from src.model import Model, GuardClauseFailure
 from src.play import PlayScreen
 
 class WildernessScreen(PlayScreen):
@@ -13,6 +15,9 @@ class WildernessScreen(PlayScreen):
         """Create a WildernessScreen object."""
         super().__init__(parent, model)
         self.commands += [
+                Command('refuel', 'Refuel', self.refuel),
+                Command('liftoff', 'Lift off to orbit', self.liftoff),
+                Command('starport', 'Fly to starport', self.starport),
                 ]
         self.commands = sorted(self.commands, key=lambda command: command.key)
 
@@ -22,4 +27,32 @@ class WildernessScreen(PlayScreen):
 
     # VIEW COMMANDS ========================================================
     # STATE TRANSITIONS ====================================================
+    # TO_DO: review Model.liftoff() for applicability
+    # TO_DO: this duplicates Starport.liftoff()
+    def liftoff(self) -> None:
+        """Move from the surface to orbit."""
+        print(f"{BOLD_BLUE}Lifting off to orbit {self.model.system_name()}.{END_FORMAT}")
+        try:
+            #print(self.model.liftoff())
+            self.parent.change_state("Orbit")
+        except GuardClauseFailure as exception:
+            print(exception)
+
+    # TO_DO: review Model.land() for applicability
+    # TO_DO: this duplicates Orbit.land()
+    def starport(self) -> None:
+        """Move from the wilderness to the starport."""
+        print(f"{BOLD_BLUE}Flying to {self.model.system_name()} starport.{END_FORMAT}")
+        try:
+            #print(self.model.land())
+            self.parent.change_state("Starport")
+        except GuardClauseFailure as exception:
+            print(exception)
+
     # ACTIONS ==============================================================
+    # TO_DO: restrict this by world hydrosphere value
+    # TO_DO: this should only supply unrefined fuel
+    def refuel(self) -> None:
+        """Perform wilderness refuelling."""
+        print(f"{BOLD_BLUE}Refuelling ship.{END_FORMAT}")
+        #print(self.model.refuel())
