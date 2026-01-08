e0ef81a8c84c887cd666f68fe1693b3ab395e8c9
josh-reynolds <josh.reynolds@frontier.com>
2026-01-08 11:58:32 -0800

    Adding test for successful damage control

--- RPGs/traveller/test/procedures_test.py
+++ RPGs/traveller/test/procedures_test.py
@@ -1,6 +1,7 @@
 """Contains tests for game procedures."""
 import unittest
-from test.mock import ControlsMock, ShipMock
+from test.mock import ControlsMock, ShipMock, CalendarMock
+from src.imperial_date import ImperialDate
 from src.model import Model
 from src.ship import RepairStatus
 
@@ -13,6 +14,7 @@ class DamageControlTestCase(unittest.TestCase):
         """Create fixtures for testing."""
         DamageControlTestCase.model = Model(ControlsMock([]))
         DamageControlTestCase.model.ship = ShipMock()
+        DamageControlTestCase.model.date = CalendarMock()
 
     def test_damage_control_with_repaired_ship(self) -> None:
         """Tests attempting to perform damage control when the ship is fully repaired."""
@@ -32,3 +34,16 @@ class DamageControlTestCase(unittest.TestCase):
         result = model.damage_control()
 
         self.assertEqual(result, "Further repairs require starport facilities.")
+
+    def test_damage_control_success(self) -> None:
+        """Tests successful damage control."""
+        model = DamageControlTestCase.model
+        model.ship.repair_status = RepairStatus.BROKEN
+        model.ship.crew[0].engineer_skill = 8        # guarantee the repair succeeds
+
+        self.assertEqual(model.date.current_date, ImperialDate(1, 1105))
+
+        result = model.damage_control()
+
+        self.assertEqual(model.date.current_date, ImperialDate(2, 1105))
+        self.assertEqual(result, "Ship partially repaired. Visit a starport for further work.")
