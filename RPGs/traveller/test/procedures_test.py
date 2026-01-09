b8e59653fbab20a15fd90338cfa6889014c29ec0
josh-reynolds <josh.reynolds@frontier.com>
2026-01-09 13:57:40 -0800

    Adding test for successful refuelling at a mid-grade starport

--- RPGs/traveller/test/procedures_test.py
+++ RPGs/traveller/test/procedures_test.py
@@ -2,6 +2,8 @@
 import unittest
 from typing import cast
 from test.mock import ControlsMock, ShipMock, CalendarMock, SystemMock
+from test.mock import ObserverMock, FinancialsMock
+from src.credits import Credits
 from src.imperial_date import ImperialDate
 from src.model import Model, GuardClauseFailure
 from src.ship import RepairStatus, FuelQuality
@@ -150,6 +152,7 @@ class RefuelTestCase(unittest.TestCase):
         RefuelTestCase.model = Model(ControlsMock([]))
         RefuelTestCase.model.ship = ShipMock()
         RefuelTestCase.model.map_hex = SystemMock()
+        RefuelTestCase.model.financials = FinancialsMock()
 
     # pylint: disable=W0212
     # W0212: access to a protected member _starport of a client class
@@ -168,3 +171,52 @@ class RefuelTestCase(unittest.TestCase):
             model.refuel()
         self.assertEqual(f"{context.exception}",
                          "No fuel is available at starport X.")
+
+    # pylint: disable=W0212
+    # W0212: access to a protected member _starport of a client class
+    def test_refuel_with_mid_facilities(self) -> None:
+        """Tests attempting to refuel at a medium-grade starport."""
+        model = RefuelTestCase.model
+        view = ObserverMock()
+        model.views = [view]
+        model.controls = ControlsMock(['y', 'n', 'n'])
+        model.financials.balance = Credits(5000)
+
+        self.assertEqual(model.fuel_level(), 0)
+        self.assertEqual(model.ship.fuel_quality, FuelQuality.REFINED)
+        self.assertEqual(len(model.financials.ledger), 0)
+
+        cast(SystemMock, model.map_hex)._starport = "C"
+        result = model.refuel()
+        self.assertEqual(view.message, "Note: only unrefined fuel available at this facility.")
+        self.assertEqual(model.controls.invocations, 1)
+
+        cast(SystemMock, model.map_hex)._starport = "D"
+        result = model.refuel()
+        self.assertEqual(view.message, "Note: only unrefined fuel available at this facility.")
+        self.assertEqual(model.controls.invocations, 2)
+
+        cast(SystemMock, model.map_hex)._starport = "C"
+        result = model.refuel()
+        # TO_DO: ObserverMock only keeps the last messsage, should accumulate a list
+        self.assertEqual(view.message, "Charging 3,000 Cr for refuelling.")
+        self.assertEqual(model.controls.invocations, 3)
+        self.assertEqual(model.fuel_level(), 30)
+        self.assertEqual(model.ship.fuel_quality, FuelQuality.UNREFINED)
+        self.assertEqual(model.balance, Credits(2000))
+        self.assertEqual(len(model.financials.ledger), 1)
+        self.assertEqual(model.financials.ledger[0],
+                         "1\t - 3,000 Cr\t - \t\t - 2,000 Cr\t - Uranus\t - refuelling")
+        self.assertEqual(result, "Your ship is fully refuelled.")
+
+        # low grade starport - no fuel
+        # medium grade starport - unrefined fuel
+        # high grade starport - refined fuel
+        # empty tanks
+        # partial tanks
+        # full tanks
+        # debit cost from balance
+        # ledger entries
+        # price per ton of fuel, unrefined vs. refined
+        # view notifications
+        # confirm/deny transaction
