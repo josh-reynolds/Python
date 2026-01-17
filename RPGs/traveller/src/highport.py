0cead574bd1054668eb2072f8c7b46c1942ddf7a
josh-reynolds <josh.reynolds@frontier.com>
2026-01-17 13:48:56 -0800

    Adding Highport.recharge(), .refuel(), .maintenance(), .flush() and .repair_ship()

--- RPGs/traveller/src/highport.py
+++ RPGs/traveller/src/highport.py
@@ -15,8 +15,13 @@ class HighportScreen(PlayScreen):
         """Create a HighportScreen object."""
         super().__init__(parent, model)
         self.commands += [
+                Command('life support', 'Recharge life support', self.recharge),
+                Command('refuel', 'Refuel', self.refuel),
                 Command('launch', 'Return to orbit', self.launch),
                 Command('starport', 'Land at the downport', self.starport),
+                Command('maintenance', 'Annual maintenance', self.maintenance),
+                Command('flush tanks', 'Flush fuel tanks', self.flush),
+                Command('repair', 'Repair ship', self.repair_ship),
                 ]
         self.commands = sorted(self.commands, key=lambda command: command.key)
 
@@ -51,3 +56,34 @@ class HighportScreen(PlayScreen):
             print(exception)
 
     # ACTIONS ==============================================================
+    # TO_DO: should this be restricted at low-facility starports (E/X)?
+    def recharge(self) -> None:
+        """Recharge the Ship's life support system."""
+        print(f"{BOLD_BLUE}Replenishing life support system.{END_FORMAT}")
+        self.model.recharge_life_support()
+
+    def refuel(self) -> None:
+        """Refuel the Ship."""
+        print(f"{BOLD_BLUE}Refuelling ship.{END_FORMAT}")
+        try:
+            print(self.model.refuel())
+        except GuardClauseFailure as exception:
+            print(exception)
+
+    def maintenance(self) -> None:
+        """Perform annual maintenance on the Ship."""
+        print(f"{BOLD_BLUE}Performing annual ship maintenance.{END_FORMAT}")
+        try:
+            print(self.model.annual_maintenance())
+        except GuardClauseFailure as exception:
+            print(exception)
+
+    def flush(self) -> None:
+        """Decontaminate the Ship's fuel tanks."""
+        print(f"{BOLD_BLUE}Flushing out fuel tanks.{END_FORMAT}")
+        print(self.model.flush())
+
+    def repair_ship(self) -> None:
+        """Fully repair damage to the Ship (Starport)."""
+        print(f"{BOLD_BLUE}Starport repairs.{END_FORMAT}")
+        print(self.model.repair_ship())
