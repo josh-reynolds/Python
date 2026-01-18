4382fe444a3e235e70e9a94b2ed8a8c9b0858ac2
josh-reynolds <josh.reynolds@frontier.com>
2026-01-18 13:21:33 -0800

    Moving starport transition commands to DownportScreen class

--- RPGs/traveller/main.py
+++ RPGs/traveller/main.py
@@ -3,13 +3,13 @@
 Game - contains the game loop and basic controller/view logic.
 """
 from src.format import BOLD_YELLOW, BOLD_RED, END_FORMAT, BOLD_GREEN
+from src.downport import DownportScreen
 from src.highport import HighportScreen
 from src.jump import JumpScreen
 from src.menu import MenuScreen
 from src.model import Model
 from src.orbit import OrbitScreen
 from src.screen import Screen
-from src.starport import StarportScreen
 from src.terminal import TerminalScreen
 from src.trade import TradeScreen
 from src.utilities import int_input, confirm_input
@@ -65,7 +65,7 @@ class Game:
             case "Orbit":
                 self.screen = OrbitScreen(self, self.screen.model)
             case "Starport":
-                self.screen = StarportScreen(self, self.screen.model)
+                self.screen = DownportScreen(self, self.screen.model)
             case "Highport":
                 self.screen = HighportScreen(self, self.screen.model)
             case "Jump":
--- RPGs/traveller/src/starport.py
+++ RPGs/traveller/src/starport.py
@@ -17,14 +17,9 @@ class StarportScreen(PlayScreen):
         self.commands += [
                 Command('life support', 'Recharge life support', self.recharge),
                 Command('refuel', 'Refuel', self.refuel),
-                Command('liftoff', 'Lift off to orbit', self.liftoff),
-                Command('highport', 'Lift off to the highport', self.highport),
-                Command('depot', 'Trade depot', self.to_depot),
-                Command('terminal', 'Passenger terminal', self.to_terminal),
                 Command('maintenance', 'Annual maintenance', self.maintenance),
                 Command('flush tanks', 'Flush fuel tanks', self.flush),
                 Command('repair', 'Repair ship', self.repair_ship),
-                Command('wilderness', 'Fly to the wilderness', self.wilderness),
                 ]
         self.commands = sorted(self.commands, key=lambda command: command.key)
 
@@ -34,50 +29,6 @@ class StarportScreen(PlayScreen):
 
     # VIEW COMMANDS ========================================================
     # STATE TRANSITIONS ====================================================
-    def liftoff(self) -> None:
-        """Move from the starport to orbit."""
-        print(f"{BOLD_BLUE}Lifting off to orbit {self.model.system_name()}.{END_FORMAT}")
-        try:
-            print(self.model.liftoff())
-            self.parent.change_state("Orbit")
-        except GuardClauseFailure as exception:
-            print(exception)
-
-    def highport(self) -> None:
-        """Move from the starport to the highport."""
-        print(f"{BOLD_BLUE}Lifting off to dock with the {self.model.system_name()} " +
-              f"highport.{END_FORMAT}")
-        try:
-            print(self.model.dock())
-            self.parent.change_state("Highport")
-        except GuardClauseFailure as exception:
-            print(exception)
-
-    def to_depot(self) -> None:
-        """Move from the starport to the trade depot."""
-        print(f"{BOLD_BLUE}Entering {self.model.system_name()} trade depot.{END_FORMAT}")
-        print(self.model.to_depot())
-        self.parent.change_state("Trade")
-
-    def to_terminal(self) -> None:
-        """Move from the starport to the passenger terminal."""
-        print(f"{BOLD_BLUE}Entering {self.model.system_name()} " +
-              f"passenger terminal.{END_FORMAT}")
-        print(self.model.to_terminal())
-        self.parent.change_state("Terminal")
-
-    # TO_DO: duplicates OrbitScreen.wilderness()
-    # pylint: disable=R0801
-    # R0801: Similar lines in 2 files
-    def wilderness(self) -> None:
-        """Fly from the starport to the wilderness."""
-        print(f"{BOLD_BLUE}Flying to the wilderness on {self.model.system_name()}.{END_FORMAT}")
-        try:
-            print(self.model.wilderness())
-            self.parent.change_state("Wilderness")
-        except GuardClauseFailure as exception:
-            print(exception)
-
     # ACTIONS ==============================================================
     # TO_DO: should this be restricted at low-facility starports (E/X)?
     def recharge(self) -> None:
