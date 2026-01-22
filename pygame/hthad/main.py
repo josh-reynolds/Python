3aeb2592b0784f4f61932b84e17616afc7eafe02
josh-reynolds <josh.reynolds@frontier.com>
2026-01-22 15:05:28 -0800

    Drawing the background sky and earth

--- pygame/hthad/main.py
+++ pygame/hthad/main.py
@@ -9,10 +9,16 @@ WIDTH = 1100
 HEIGHT = 850
 TITLE = "How to Host a Dungeon"
 
+GROUND_LEVEL = HEIGHT // 5
+
+SKY = (36, 87, 192)
+GROUND = (81, 76, 34)
+
 def update():
     pass
 
 def draw():
-    pass
+    screen.draw.rect(0, 0, WIDTH, GROUND_LEVEL, SKY, 0)
+    screen.draw.rect(0, GROUND_LEVEL, WIDTH, HEIGHT, GROUND, 0)
 
 run()
