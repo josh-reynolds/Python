291acb5877c3830d5763606176569d114d016ae6
josh-reynolds <josh.reynolds@frontier.com>
2026-01-22 14:54:40 -0800

    Starting setup for 'How to Host a Dungeon'

---
+++ pygame/hthad/main.py
@@ -0,0 +1,18 @@
+"""Play Tony Dowler's 'How to Host a Dungeon'."""
+import math
+import os.path
+import pygame
+from engine import screen, run
+from screen_matrix import push_matrix, pop_matrix, line, rotate, translate
+
+WIDTH = 1100
+HEIGHT = 850
+TITLE = "How to Host a Dungeon"
+
+def update():
+    pass
+
+def draw():
+    pass
+
+run()
