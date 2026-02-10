3b20de22cbea1fbd71f2121b9d3ce9af36741ff9
josh-reynolds <josh.reynolds@frontier.com>
2026-02-10 11:56:53 -0800

    Adding tests for parallel and collinear line segments

--- pygame/intersections/unit_tests.py
+++ pygame/intersections/unit_tests.py
@@ -13,23 +13,53 @@ class SegmentIntersectionTestCase(unittest.TestCase):
         self.assertTrue(segments_intersect(line_1, line_2))
 
     def test_no_intersect_within_segments(self) -> None:
-        """Tests simple line segment intersection."""
+        """Tests non-intersecting non-parallel lines."""
         line_1 = (PVector(100, 100), PVector(200, 100))
         line_2 = (PVector(250, 50), PVector(250, 150))
         self.assertFalse(segments_intersect(line_1, line_2))
 
     def test_touch_at_endpoint(self) -> None:
-        """Tests simple line segment intersection."""
+        """Tests line segments touching at one endpoint."""
         line_1 = (PVector(100, 100), PVector(200, 100))
         line_2 = (PVector(150, 50), PVector(150, 100))
         self.assertTrue(segments_intersect(line_1, line_2))
 
+    def test_parallel_lines(self) -> None:
+        """Tests parallel line (non-) intersection."""
+        line_1 = (PVector(100, 100), PVector(200, 100))
+        line_2 = (PVector(100, 200), PVector(200, 200))
+        self.assertFalse(segments_intersect(line_1, line_2))
+
+    def test_non_overlapping_collinear_lines(self) -> None:
+        """Tests collinear line segments that do not overlap."""
+        line_1 = (PVector(100, 100), PVector(200, 100))
+        line_2 = (PVector(300, 100), PVector(400, 100))
+        self.assertFalse(segments_intersect(line_1, line_2))
+
+    def test_collinear_enpoints_touching(self) -> None:
+        """Tests collinear line segments that touch at an endpoint."""
+        line_1 = (PVector(100, 100), PVector(200, 100))
+        line_2 = (PVector(200, 100), PVector(300, 100))
+        self.assertTrue(segments_intersect(line_1, line_2))
+
+    def test_overlapping_collinear_lines(self) -> None:
+        """Tests collinear line segments that overlap."""
+        line_1 = (PVector(100, 100), PVector(200, 100))
+        line_2 = (PVector(150, 100), PVector(250, 100))
+        self.assertTrue(segments_intersect(line_1, line_2))
+
+    def test_collinear_lines_entirely_overlapping(self) -> None:
+        """Tests line segment containing a collinear line segment."""
+        line_1 = (PVector(100, 100), PVector(200, 100))
+        line_2 = (PVector(125, 100), PVector(150, 100))
+        self.assertTrue(segments_intersect(line_1, line_2))
+
 # line-line intersections ----------------
-# [  ] parallel
-# [  ] collinear - no overlap
-# [  ] collinear - touch at endpoint
-# [  ] collinear - overlap
-# [  ] collinear - line contained within line
+# [OK] parallel
+# [OK] collinear - no overlap
+# [OK] collinear - touch at endpoint
+# [OK] collinear - overlap
+# [OK] collinear - line contained within line
 # [OK] intersect - touch at an endpoint
 # [OK] intersect within segments
 # [OK] does not intersect within segments
