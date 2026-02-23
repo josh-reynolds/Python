b2f12c72c94cdaa511e804a9d282c95e39c0e20c
josh-reynolds <josh.reynolds@frontier.com>
2026-02-23 15:40:49 -0800

    Replacing imperative polygon definition with a class

--- pygame/strata/main.py
+++ pygame/strata/main.py
@@ -1,27 +1,32 @@
 """Draw randomly varying layers across the screen."""
 from random import randint
+from typing import Tuple
 from engine import screen, run
 # pylint: disable=W0611
 # W0611: Unused TITLE imported from constants (unused-import)
 from constants import WIDTH, HEIGHT, TITLE, STRATA_HEIGHT
 
-depth = 150
-points = []
-points.append((0,depth))
-for i in range(5):
-    points.append(((i+1) * WIDTH//5, depth + randint(-10,10)))
-points.append((WIDTH,depth))
-points.append((WIDTH,HEIGHT))
-points.append((0,HEIGHT))
 
-depth = 250
-points_2 = []
-points_2.append((0,depth))
-for i in range(5):
-    points_2.append(((i+1) * WIDTH//5, depth + randint(-10,10)))
-points_2.append((WIDTH,depth))
-points_2.append((WIDTH,HEIGHT))
-points_2.append((0,HEIGHT))
+class Stratum():
+    """Represents a geologic layer underground."""
+
+    def __init__(self, depth: int, color: Tuple[int,int,int]) -> None:
+        """Create an instance of a Stratum object."""
+        self.depth = depth
+        self.color = color
+
+        self.points = []
+        self.points.append((0,self.depth))
+        for i in range(5):
+            self.points.append(((i+1) * WIDTH//5, self.depth + randint(-10,10)))
+        self.points.append((WIDTH,self.depth))
+        self.points.append((WIDTH,HEIGHT))
+        self.points.append((0,HEIGHT))
+
+
+strata = []
+strata.append(Stratum(150, (105,99,39)))
+strata.append(Stratum(250, (137,131,75)))
 
 def update() -> None:
     """Update game state once per frame."""
@@ -30,8 +35,8 @@ def draw() -> None:
     """Draw the game screen once per frame."""
     screen.fill((80,80,220))
 
-    screen.draw.polygon(points, (105,99,39), 0)
-    screen.draw.polygon(points_2, (137,131,75), 0)
+    screen.draw.polygon(strata[0].points, strata[0].color, 0)
+    screen.draw.polygon(strata[1].points, strata[1].color, 0)
 
 
 run()
