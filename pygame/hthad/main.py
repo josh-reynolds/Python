d6ff68c821ca34a81f197a78ab0408d272f0110b
josh-reynolds <josh.reynolds@frontier.com>
2026-03-04 13:07:40 -0800

    Adding 'Exploratory Shaft' construction event

--- pygame/hthad/main.py
+++ pygame/hthad/main.py
@@ -521,6 +521,20 @@ class CivilizationAge():
                     case 5:
                         print("Exploratory Shaft")
                         # This can trigger end - see case 10
+                        all_rooms = self.mine_start.get_all_connected_locations()
+                        shaft_x = self.mine_start.coordinate.x
+                        shaft_rooms = [m for m in all_rooms if m.coordinate.x == shaft_x]
+                        shaft_rooms.sort(key=lambda room: room.coordinate.y)
+                        shaft_bottom = shaft_rooms.pop()
+
+                        new_location = PVector(shaft_bottom.coordinate.x, 
+                                               shaft_bottom.coordinate.y + FINGER)
+                        new_room = create_location(Room, new_location)
+                        new_room.name = "Outpost"
+                        new_room.color = shaft_bottom.color
+                        shaft_bottom.add_neighbor(new_room)
+                        new_locations.append(new_room)
+
                     case 6:
                         print("Hall Expansion")
                     case 7:
