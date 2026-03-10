cc033da5ebfbf5a160294f9fbfe57aa496184508
josh-reynolds <josh.reynolds@frontier.com>
2026-03-10 13:36:09 -0700

    Moving PrimordialAge and support functions to separate module

--- pygame/hthad/main.py
+++ pygame/hthad/main.py
@@ -1,5 +1,4 @@
 """Play Tony Dowler's 'How to Host a Dungeon'."""
-import math
 from random import randint, choice, shuffle
 from typing import List, Tuple, Callable
 from pygame import Rect
@@ -15,9 +14,11 @@ from landscape import UndergroundRiver, Stratum
 from constants import WIDTH, HEIGHT, TITLE, BEAD, GROUND_LEVEL, CAVERN
 from constants import FINGER, CREATURE, EVENT, STRATA_HEIGHT, DWARF, TREASURE
 from constants import ROOM_SPACING, GROUND, SKY, BORDER, WATER, SHOW_LABELS
+from primordial_age import PrimordialAge, create_location
 
 locations = []
 
+# TO_DO: touches global locations list
 def check_for_connections(room: Location) -> None:
     """Test whether a newly-added room collides with an existing one, and link up if so."""
     cavities = [l for l in locations if isinstance(l, Location)]
@@ -28,13 +29,6 @@ def check_for_connections(room: Location) -> None:
             print(f"Intersection detected: {room} - {location}")
             room.add_neighbor(location)
 
-def create_location(location_type: Callable, coordinate: PVector,
-                    size: Tuple[int,int]=(BEAD,BEAD)) -> Location:
-    """Create a new location and add to the list."""
-    new_location = location_type(coordinate=coordinate, size=size)
-    check_for_connections(new_location)
-    return new_location
-
 def get_all_entities() -> List[Entity]:
     """Return a list of all Entities on the map."""
     entities = []
@@ -42,98 +36,6 @@ def get_all_entities() -> List[Entity]:
         if isinstance(location, Location) and location.contents:
             entities.append(location.contents)
     return entities
-
-def natural_cavern_factory() -> Cavern:
-    """Generate a natural cavern."""
-    coordinate = get_random_underground_location()
-
-    cavern = create_location(Cavern, coordinate)
-    cavern.color = CAVERN
-
-    detail = randint(1,6)
-    match detail:
-        case 1:
-            cavern.tunnel = True
-            cavern.tilt = randint(-FINGER//2, FINGER//2)
-        case 2:
-            cavern.contents = Entity(f"Plague {randint(1,4)}", cavern, EVENT)
-        case 3:
-            cavern.contents = Entity(f"Gemstones {randint(1,4)}", cavern, TREASURE)
-        case 4:
-            pass
-        case 5:
-            cavern.contents = Entity("Primordial Beasts", cavern, CREATURE)
-        case 6:
-            cavern.contents = Entity("Fate", cavern, EVENT)
-
-    return cavern
-
-def get_orbital_point(origin: PVector, radius: int, angle: int) -> PVector:
-    """Generate a point on a circle of given radius and angle around an origin."""
-    new_x = radius * math.cos(math.radians(angle)) + origin.x
-    new_y = radius * math.sin(math.radians(angle)) + origin.y
-    return PVector(new_x, new_y)
-
-def cave_complex_factory() -> List[Cavern]:
-    """Generate a cave complex."""
-    radius = BEAD
-    sites = []
-
-    first_point = get_random_underground_location()
-    cavern1 = create_location(Cavern, first_point)
-    cavern1.color = CAVERN
-    cavern1.contents = Entity("Primordial Beasts", cavern1, CREATURE)
-    sites.append(cavern1)
-
-    angle_1 = randint(0,359)
-    second_point = get_orbital_point(first_point, 3 * radius, angle_1)
-    cavern2 = create_location(Cavern, second_point)
-    cavern2.color = CAVERN
-    cavern2.contents = Entity("Primordial Beasts", cavern2, CREATURE)
-    sites.append(cavern2)
-
-    angle_2 = angle_1 + randint(70,290)
-    third_point = get_orbital_point(first_point, 3 * radius, angle_2)
-    cavern3 = create_location(Cavern, third_point)
-    cavern3.color = CAVERN
-    cavern3.contents = Entity("Primordial Beasts", cavern3, CREATURE)
-    sites.append(cavern3)
-
-    sites[0].add_neighbor(sites[1])
-    sites[0].add_neighbor(sites[2])
-
-    return sites
-
-# TO_DO: name the wyrm
-def ancient_wyrm_factory() -> List[Cavern]:
-    """Generate a cavern containing an ancient wyrm."""
-    radius = BEAD
-    sites = []
-
-    first_point = get_random_underground_location()
-    cavern1 = create_location(Cavern, first_point)
-    cavern1.color = CAVERN
-    cavern1.contents = Entity("Ancient Wyrm", cavern1, CREATURE)
-    sites.append(cavern1)
-
-    angle_1 = randint(0,359)
-    second_point = get_orbital_point(first_point, radius * 1.5, angle_1)
-    cavern2 = create_location(Cavern, second_point)
-    cavern2.color = CAVERN
-    cavern2.contents = Entity("Wyrm Treasure", cavern2, TREASURE)
-    sites.append(cavern2)
-
-    sites[0].add_neighbor(sites[1])
-
-    return sites
-
-def add_caverns() -> None:
-    """Generate a random number of Caverns."""
-    locations.append(natural_cavern_factory())
-    cavern_count = 1
-    while randint(1,6) < 6 and cavern_count < 6:
-        locations.append(natural_cavern_factory())
-        cavern_count += 1
 
 def dwarf_mine_factory(x_location: int, depth: int,
                        target_mineral: GoldVein | Mithril) -> List[Room]:
@@ -175,7 +77,6 @@ def get_y_at_x(start: PVector, end: PVector, x_coord: int) -> int:
 
     return slope * x_coord + intercept
 
-
 colors = [(105,99,39),
           (137,131,75),
           (87,84,57),
@@ -189,7 +90,6 @@ strata = []
 for i in range(6):
     strata.append(Stratum(150 + i * 120, colors[i]))
 
-
 # TO_DO: need to keep track of yearly events
 
 # Spring ~~~~~~~~
@@ -197,7 +97,6 @@ for i in range(6):
 # draw a new treasure room for each, or place in existing empty rooms
 # for each treasure gathered, draw a barracks and put a dwarf creature in it,
 # away from the mines
-
 
 # TO_DO: don't store all locations in locations list, we just need
 #        one node (i.e. location) for each graph
@@ -309,52 +208,6 @@ def get_candidate_room(parents: List[Location], room_name: str,
 
     print("No viable locations found!")
     return None
-
-
-class PrimordialAge():
-    """Manages creation of the landscape and entities during the Primordial Age."""
-
-    def __init__(self) -> None:
-        """Create an instance of a PrimordialAge object."""
-        self.rolls = 0
-        self.name = "Primordial Age"
-
-    def update(self) -> List:
-        """Return the next generated map location."""
-        print("PrimordialAge.update()")
-
-        new_locations = []
-
-        check = randint(0,6)
-        match check:
-            case 0:
-                # TO_DO: for testing
-                #new_locations.append(Mithril())
-                new_locations.append(GoldVein())
-            case 1 | 2:
-                add_caverns()
-            case 3:
-                new_locations.append(GoldVein())
-            case 4:
-                new_locations += cave_complex_factory()
-            case 5:
-                river = UndergroundRiver()
-                new_locations.append(river)
-                for coord in river.caves:
-                    new_cavern = create_location(Cavern, coord)
-                    new_cavern.color = CAVERN
-                    new_locations.append(new_cavern)
-            case 6:
-                new_locations += ancient_wyrm_factory()
-            # TO_DO: there is an additional choice for natural disasters,
-            #        implement when we come to that rule
-
-        self.rolls += 1
-        return new_locations
-
-    def is_done(self) -> bool:
-        """Return whether the PrimodialAge has completed or not."""
-        return self.rolls > 2
 
 def get_mine_start_location(target_mineral: Mithril | GoldVein) -> Tuple[int, int]:
     """Determine where to start the dwarf mine.
---
+++ pygame/hthad/primordial_age.py
@@ -0,0 +1,156 @@
+"""Contains the PrimordialAge class and supporting functions."""
+import math
+from random import randint
+from typing import Callable, Tuple, List
+from pvector import PVector
+from constants import CAVERN, CREATURE, BEAD, FINGER, EVENT, TREASURE
+from entity import Entity
+from landscape import Mithril, GoldVein, UndergroundRiver, get_random_underground_location
+from location import Cavern, Location
+
+def natural_cavern_factory() -> Cavern:
+    """Generate a natural cavern."""
+    coordinate = get_random_underground_location()
+
+    cavern = create_location(Cavern, coordinate)
+    cavern.color = CAVERN
+
+    detail = randint(1,6)
+    match detail:
+        case 1:
+            cavern.tunnel = True
+            cavern.tilt = randint(-FINGER//2, FINGER//2)
+        case 2:
+            cavern.contents = Entity(f"Plague {randint(1,4)}", cavern, EVENT)
+        case 3:
+            cavern.contents = Entity(f"Gemstones {randint(1,4)}", cavern, TREASURE)
+        case 4:
+            pass
+        case 5:
+            cavern.contents = Entity("Primordial Beasts", cavern, CREATURE)
+        case 6:
+            cavern.contents = Entity("Fate", cavern, EVENT)
+
+    return cavern
+
+def add_caverns() -> List[Location]:
+    """Generate a random number of Caverns."""
+    new_locations = [natural_cavern_factory()]
+    cavern_count = 1
+    while randint(1,6) < 6 and cavern_count < 6:
+        new_locations.append(natural_cavern_factory())
+        cavern_count += 1
+    return new_locations
+
+def create_location(location_type: Callable, coordinate: PVector,
+                    size: Tuple[int,int]=(BEAD,BEAD)) -> Location:
+    """Create a new location and add to the list."""
+    new_location = location_type(coordinate=coordinate, size=size)
+    # TO_DO: commented out while check_for_connections is fixed up
+    #check_for_connections(new_location)
+    return new_location
+
+def get_orbital_point(origin: PVector, radius: int, angle: int) -> PVector:
+    """Generate a point on a circle of given radius and angle around an origin."""
+    new_x = radius * math.cos(math.radians(angle)) + origin.x
+    new_y = radius * math.sin(math.radians(angle)) + origin.y
+    return PVector(new_x, new_y)
+
+def cave_complex_factory() -> List[Cavern]:
+    """Generate a cave complex."""
+    radius = BEAD
+    sites = []
+
+    first_point = get_random_underground_location()
+    cavern1 = create_location(Cavern, first_point)
+    cavern1.color = CAVERN
+    cavern1.contents = Entity("Primordial Beasts", cavern1, CREATURE)
+    sites.append(cavern1)
+
+    angle_1 = randint(0,359)
+    second_point = get_orbital_point(first_point, 3 * radius, angle_1)
+    cavern2 = create_location(Cavern, second_point)
+    cavern2.color = CAVERN
+    cavern2.contents = Entity("Primordial Beasts", cavern2, CREATURE)
+    sites.append(cavern2)
+
+    angle_2 = angle_1 + randint(70,290)
+    third_point = get_orbital_point(first_point, 3 * radius, angle_2)
+    cavern3 = create_location(Cavern, third_point)
+    cavern3.color = CAVERN
+    cavern3.contents = Entity("Primordial Beasts", cavern3, CREATURE)
+    sites.append(cavern3)
+
+    sites[0].add_neighbor(sites[1])
+    sites[0].add_neighbor(sites[2])
+
+    return sites
+
+# TO_DO: name the wyrm
+def ancient_wyrm_factory() -> List[Cavern]:
+    """Generate a cavern containing an ancient wyrm."""
+    radius = BEAD
+    sites = []
+
+    first_point = get_random_underground_location()
+    cavern1 = create_location(Cavern, first_point)
+    cavern1.color = CAVERN
+    cavern1.contents = Entity("Ancient Wyrm", cavern1, CREATURE)
+    sites.append(cavern1)
+
+    angle_1 = randint(0,359)
+    second_point = get_orbital_point(first_point, radius * 1.5, angle_1)
+    cavern2 = create_location(Cavern, second_point)
+    cavern2.color = CAVERN
+    cavern2.contents = Entity("Wyrm Treasure", cavern2, TREASURE)
+    sites.append(cavern2)
+
+    sites[0].add_neighbor(sites[1])
+
+    return sites
+
+
+class PrimordialAge():
+    """Manages creation of the landscape and entities during the Primordial Age."""
+
+    def __init__(self) -> None:
+        """Create an instance of a PrimordialAge object."""
+        self.rolls = 0
+        self.name = "Primordial Age"
+
+    def update(self) -> List:
+        """Return the next generated map location."""
+        print("PrimordialAge.update()")
+
+        new_locations = []
+
+        check = randint(0,6)
+        match check:
+            case 0:
+                # TO_DO: for testing
+                #new_locations.append(Mithril())
+                new_locations.append(GoldVein())
+            case 1 | 2:
+                new_locations += add_caverns()
+            case 3:
+                new_locations.append(GoldVein())
+            case 4:
+                new_locations += cave_complex_factory()
+            case 5:
+                river = UndergroundRiver()
+                new_locations.append(river)
+                for coord in river.caves:
+                    new_cavern = create_location(Cavern, coord)
+                    new_cavern.color = CAVERN
+                    new_locations.append(new_cavern)
+            case 6:
+                new_locations += ancient_wyrm_factory()
+            # TO_DO: there is an additional choice for natural disasters,
+            #        implement when we come to that rule
+
+        self.rolls += 1
+        return new_locations
+
+    def is_done(self) -> bool:
+        """Return whether the PrimordialAge has completed or not."""
+        return self.rolls > 2
